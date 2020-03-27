14e3cde04cbcc2dc6e4977a9656d6820d00ef7d4
Kristina Head <kristina_head@icloud.com>
2020-03-27 14:54:32 +0100

    Time complexity analysis

--- number-of-steps-to-reduce-a-number-to-zero/Solution.java
+++ number-of-steps-to-reduce-a-number-to-zero/Solution.java
@@ -1,15 +1,15 @@
 class Solution {
-  public int numberOfSteps(int num) {
-    int count = 0;
+  public int numberOfSteps(int num) { // O(1)
+    int count = 0; // O(1)
     
-    while (num != 0) {
-      count++;
-      if (num % 2 == 0) {
-        num /= 2;
+    while (num != 0) { // O(1)
+      count++; // O(1)
+      if (num % 2 == 0) { // O(1)
+        num /= 2; // O(1)
       } else {
-        num -= 1;
+        num -= 1; // O(1)
       }
     }
-    return count;
+    return count; // O(1)
   }
 }
