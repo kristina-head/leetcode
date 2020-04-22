211c97ee46a2636807f778506be0236fc6bb81cc
Kristina Head <kristina_head@icloud.com>
2020-04-22 19:16:57 +0200

    Solution with time complexity

---
+++ unique-number-of-occurances/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) { // O(N + M)
+        Map<Integer, Integer> frequency = new HashMap<>(); // O(1)
+        
+        for (int i = 0; i < arr.length; i++) { // O(N)
+            if (frequency.containsKey(arr[i])) { // O(1)
+                frequency.put(arr[i], frequency.get(arr[i]) + 1); // O(1)
+            } else {
+                frequency.put(arr[i], 1); // O(1)
+            }
+        }
+        
+        Set<Integer> values = new HashSet<>(frequency.values()); // O(M)
+        
+        if (values.size() != frequency.values().size()) { // O(1)
+            return false; // O(1)
+        }
+        return true; // O(1)
+    }
+}
