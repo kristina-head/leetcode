e95143fc56c158e887f2c78123ca788a0f2ec9d8
Kristina Head <1kristinahead@gmail.com>
2020-05-10 12:13:33 +0200

    Time complexity analysis

--- remove-outermost-parenthesis/Solution.java
+++ remove-outermost-parenthesis/Solution.java
@@ -1,22 +1,22 @@
 class Solution {
-    public String removeOuterParentheses(String S) {
-        int bracket = 0;
-        StringBuilder innerParentheses = new StringBuilder();
+    public String removeOuterParentheses(String S) { // O(N)
+        int bracket = 0; // O(1)
+        StringBuilder innerParentheses = new StringBuilder(); // O(1)
         
-        for (char c : S.toCharArray()) {
-            if (c == '(') {
-                bracket++;
-                if (bracket > 1) {
-                    innerParentheses.append(c);
+        for (char c : S.toCharArray()) { // O(N)
+            if (c == '(') { // O(1)
+                bracket++; // O(1)
+                if (bracket > 1) { // O(1)
+                    innerParentheses.append(c); // O(1)
                 }
             } else {
-                bracket--;
-                if (bracket > 0) {
-                    innerParentheses.append(c);
+                bracket--; // O(1)
+                if (bracket > 0) { // O(1)
+                    innerParentheses.append(c); // O(1)
                 }
             }
         }
         
-        return innerParentheses.toString();
+        return innerParentheses.toString(); // O(N)
     }
 }
