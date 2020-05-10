1c1c2c187b9778e21f7b7c4c0b53d16cb50a09c2
Kristina Head <1kristinahead@gmail.com>
2020-05-10 13:32:26 +0200

    Time complexity analysis

--- count-negative-numbers-in-a-sorted-matrix/Solution.java
+++ count-negative-numbers-in-a-sorted-matrix/Solution.java
@@ -1,24 +1,24 @@
 class Solution {
-    public int countNegatives(int[][] grid) {
-        int negatives = 0;
+    public int countNegatives(int[][] grid) { // O(N log N)
+        int negatives = 0; // O(1)
         
-        for (int i = 0; i < grid.length; i++) {
-            int start = 0;
-            int end = grid[i].length - 1;
+        for (int i = 0; i < grid.length; i++) { // O(N)
+            int start = 0; // O(1)
+            int end = grid[i].length - 1; // O(1)
             
-            while (start <= end) {
-                int mid = start + (end - start) / 2;
-                if (grid[i][mid] < 0) {
-                    end = mid - 1;
+            while (start <= end) { // O(log N)
+                int mid = start + (end - start) / 2; // O(1)
+                if (grid[i][mid] < 0) { // O(1)
+                    end = mid - 1; // O(1)
                 } else {
-                    start = mid + 1;
+                    start = mid + 1; // O(1)
                 }
             }
             
-            negatives += (grid[i].length - start);
-            end = start;
+            negatives += (grid[i].length - start); // O(1)
+            end = start; // O(1)
         }
         
-        return negatives;
+        return negatives; // O(1)
     }   
 }
