71c8a0e4ea6375f323eafd2387b86b089edb9188
Kristina Head <1kristinahead@gmail.com>
2020-05-13 09:54:55 +0200

    Time complexity analysis

--- unique-morse-code-words/Solution.java
+++ unique-morse-code-words/Solution.java
@@ -1,16 +1,16 @@
 class Solution {
-    public int uniqueMorseRepresentations(String[] words) {
-        String[] morse = new String[] {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
-        Set<String> uniqueMorseWords = new HashSet<>();
+    public int uniqueMorseRepresentations(String[] words) { // O(N)
+        String[] morse = new String[] {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."}; // O(1)
+        Set<String> uniqueMorseWords = new HashSet<>(); // O(1)
         
-        for (String word : words) {
-            StringBuilder code = new StringBuilder();
-            for (char letter : word.toCharArray()) {
-                code.append(morse[letter - 'a']);
+        for (String word : words) { // O(M)
+            StringBuilder code = new StringBuilder(); // O(1)
+            for (char letter : word.toCharArray()) { // O(N)
+                code.append(morse[letter - 'a']); // O(1)
             }
-            uniqueMorseWords.add(code.toString());
+            uniqueMorseWords.add(code.toString()); // O(1)
         }
         
-        return uniqueMorseWords.size();
+        return uniqueMorseWords.size(); // O(1)
     }
 }
