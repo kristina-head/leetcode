cfb3138e2a3e92b2b16475e256b6846d1e1dc63b
Kristina Head <1kristinahead@gmail.com>
2020-05-14 13:55:05 +0200

    Time complexity analysis

--- find-n-unique-integers-that-sum-up-to-zero/Solution.java
+++ find-n-unique-integers-that-sum-up-to-zero/Solution.java
@@ -1,11 +1,11 @@
 class Solution {
-    public int[] sumZero(int n) {
-        int[] uniqueInts = new int[n];
+    public int[] sumZero(int n) { // O(N)
+        int[] uniqueInts = new int[n]; // O(N)
         
-        for (int i = 0; i < n; i++) {
-            uniqueInts[i] = ((i * 2) - n) + 1;
+        for (int i = 0; i < n; i++) { // O(N)
+            uniqueInts[i] = ((i * 2) - n) + 1; // O(1)
         }
         
-        return uniqueInts;
+        return uniqueInts; // O(1)
     }
 }
