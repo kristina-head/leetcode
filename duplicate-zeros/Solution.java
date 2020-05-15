9016bfef72de2644385767cd9dd9f8b303ed0960
Kristina Head <1kristinahead@gmail.com>
2020-05-15 12:42:52 +0200

    Time complexity analysis

--- duplicate-zeros/Solution.java
+++ duplicate-zeros/Solution.java
@@ -1,11 +1,11 @@
 class Solution {
-    public void duplicateZeros(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0) {
-                for (int j = arr.length - 2; j >= i; j--){
-                    arr[j + 1] = arr[j];
+    public void duplicateZeros(int[] arr) { // O(N)
+        for (int i = 0; i < arr.length; i++) { // O(N)
+            if (arr[i] == 0) { // O(1)
+                for (int j = arr.length - 2; j >= i; j--){ // O(N)
+                    arr[j + 1] = arr[j]; // O(1)
                 }
-                i++;
+                i++; // O(1)
             }
         }
     }
