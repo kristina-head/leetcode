843434e45f5dbee8fdbcdac18e08bec1ee5fa227
Kristina Head <1kristinahead@gmail.com>
2020-05-15 09:38:42 +0200

    Time complexity analysis

--- squares-of-a-sorted-array/Solution.java
+++ squares-of-a-sorted-array/Solution.java
@@ -1,11 +1,11 @@
 class Solution {
-    public int[] sortedSquares(int[] A) {
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A[i] * A[i]; 
+    public int[] sortedSquares(int[] A) { // O(N log N)
+        for (int i = 0; i < A.length; i++) { // O(N)
+            A[i] = A[i] * A[i]; // O(1)
         }
         
-        Arrays.sort(A);
+        Arrays.sort(A); // O(N log N)
                
-        return A;
+        return A; // O(1)
     }
 }
