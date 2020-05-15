34491d8871940674726a5dbee4e8292bf206d17b
Kristina Head <1kristinahead@gmail.com>
2020-05-15 14:45:25 +0200

    Time complexity analysis

--- duplicate-zeros/ImprovedSolution.java
+++ duplicate-zeros/ImprovedSolution.java
@@ -1,26 +1,26 @@
 class ImprovedSolution {
-    public void duplicateZeros(int[] arr) {
-        int possibleDups = 0;
-        int length = arr.length - 1;
+    public void duplicateZeros(int[] arr) { // O(N)
+        int possibleDups = 0; // O(1)
+        int length = arr.length - 1; // O(1)
 
-        for (int left = 0; left <= length - possibleDups; left++) {
-            if (arr[left] == 0) {  
-                if (left == length - possibleDups) {
-                    arr[length] = 0;
-                    length -= 1;
+        for (int left = 0; left <= length - possibleDups; left++) { // O(N)
+            if (arr[left] == 0) { // O(1)
+                if (left == length - possibleDups) { // O(1)
+                    arr[length] = 0; // O(1)
+                    length -= 1; // O(1)
                     break;
                 }
-                possibleDups++;
+                possibleDups++; // O(1)
             }
         }
 
-        for (int i = length - possibleDups; i >= 0; i--) {
-            if (arr[i] == 0) {
-                arr[i + possibleDups] = 0;
-                possibleDups--;
-                arr[i + possibleDups] = 0;
+        for (int i = length - possibleDups; i >= 0; i--) { // O(N)
+            if (arr[i] == 0) { // O(1)
+                arr[i + possibleDups] = 0; // O(1)
+                possibleDups--; // O(1)
+                arr[i + possibleDups] = 0; // O(1)
             } else {
-                arr[i + possibleDups] = arr[i];
+                arr[i + possibleDups] = arr[i]; // O(1)
             }
         }
     }
