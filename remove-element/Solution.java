46cec69016e143d99f3a9be9aa6c3bb3084e47c4
Kristina Head <1kristinahead@gmail.com>
2020-05-17 18:21:42 +0200

    Time complexity analysis

--- remove-element/Solution.java
+++ remove-element/Solution.java
@@ -1,14 +1,14 @@
 class Solution {
-    public int removeElement(int[] nums, int val) {
-        int j = 0;
+    public int removeElement(int[] nums, int val) { // O(N)
+        int j = 0; // O(1)
         
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[j] = nums[i];
-                j++;
+        for (int i = 0; i < nums.length; i++) { // O(N)
+            if (nums[i] != val) { // O(1)
+                nums[j] = nums[i]; // O(1)
+                j++; // O(1)
             }
         }
         
-        return j;
+        return j; // O(1)
     }
 }
