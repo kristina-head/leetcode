b9f37428a33c787676866d0cde480dc8562dec8e
Kristina Head <1kristinahead@gmail.com>
2020-05-17 18:50:23 +0200

    Time complexity analysis

--- remove-duplicates-from-sorted-array/Solution.java
+++ remove-duplicates-from-sorted-array/Solution.java
@@ -1,14 +1,14 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        int j = 0;
+    public int removeDuplicates(int[] nums) { // O(N)
+        int j = 0; // O(1)
             
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[j]) {
-                j++;
-                nums[j] = nums[i];
+        for (int i = 1; i < nums.length; i++) { // O(N)
+            if (nums[i] != nums[j]) { // O(1)
+                j++; // O(1)
+                nums[j] = nums[i]; // O(1)
             }
         }
         
-        return j + 1;
+        return j + 1; // O(1)
     }
 }
