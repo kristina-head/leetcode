fcd85f385b7025f38710c541a7ca3decc32eac8c
Kristina Head <1kristinahead@gmail.com>
2020-05-26 11:41:00 +0200

    Time complexity analysis

--- sort-array-by-parity/Solution.java
+++ sort-array-by-parity/Solution.java
@@ -1,24 +1,24 @@
 class Solution {
-    public int[] sortArrayByParity(int[] A) {
-        int i = 0;
-        int j = A.length - 1;
+    public int[] sortArrayByParity(int[] A) { // O(N)
+        int i = 0; // O(1)
+        int j = A.length - 1; // O(1)
         
-        while (i < j) {
-            if (A[i] % 2 > A[j] % 2) {
-                int temp = A[i];
-                A[i] = A[j];
-                A[j] = temp;
+        while (i < j) { // O(N)
+            if (A[i] % 2 > A[j] % 2) { // O(1)
+                int temp = A[i]; // O(1)
+                A[i] = A[j]; // O(1)
+                A[j] = temp; // O(1)
             }
 
-            if (A[i] % 2 == 0) {
-                i++;
+            if (A[i] % 2 == 0) { // O(1)
+                i++; // O(1)
             }
             
-            if (A[j] % 2 == 1) {
-                j--;
+            if (A[j] % 2 == 1) { // O(1)
+                j--; // O(1)
             }
         }
 
-        return A;
+        return A; // O(1)
     }
 }
