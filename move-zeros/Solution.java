f1b66f360aeb7c849ed701d2e9e569e913270ef7
Kristina Head <1kristinahead@gmail.com>
2020-05-26 11:01:19 +0200

    Time complexity analysis

--- move-zeros/Solution.java
+++ move-zeros/Solution.java
@@ -1,15 +1,15 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int index = 0;      
+    public void moveZeroes(int[] nums) { // O(N)
+        int index = 0; // O(1)   
         
-        for (int num : nums) {
-            if (num != 0) {
-                nums[index++] = num; 
+        for (int num : nums) { // O(N)
+            if (num != 0) { // O(1)
+                nums[index++] = num; // O(1)
             }
         }
         
-        for (int i = index; i < nums.length; i++) {
-            nums[i] = 0;
+        for (int i = index; i < nums.length; i++) { // O(N)
+            nums[i] = 0; // O(1)
         }
     }
 }
