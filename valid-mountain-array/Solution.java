ff9e7ab4f19ba4c480f07e4268228c433dbe3ff9
Kristina Head <1kristinahead@gmail.com>
2020-05-26 10:04:08 +0200

    Time complexity analysis

--- valid-mountain-array/Solution.java
+++ valid-mountain-array/Solution.java
@@ -1,18 +1,18 @@
 class Solution {
-    public boolean validMountainArray(int[] A) {
-        boolean increase = false;
-        boolean decrease = false;
+    public boolean validMountainArray(int[] A) { // O(N)
+        boolean increase = false; // O(1)
+        boolean decrease = false; // O(1)
         
-        for (int i = 0; i < A.length - 1; i++) {
-            if (!decrease && A[i] < A[i+1]) {
-                increase = true;
-            } else if (increase && A[i] > A[i+1]) {
-                decrease = true;
+        for (int i = 0; i < A.length - 1; i++) { // O(N)
+            if (!decrease && A[i] < A[i+1]) { // O(1)
+                increase = true; // O(1)
+            } else if (increase && A[i] > A[i+1]) { // O(1)
+                decrease = true; // O(1)
             } else {
-                return false;
+                return false; // O(1)
             }
         }
         
-        return increase && decrease;
+        return increase && decrease; // O(1)
     }
 }
