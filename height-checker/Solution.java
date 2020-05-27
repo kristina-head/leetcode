b1553775f2d3fb55969d48fbde6c2bdd1762efa4
Kristina Head <1kristinahead@gmail.com>
2020-05-27 09:41:26 +0200

    Time complexity analysis

--- height-checker/Solution.java
+++ height-checker/Solution.java
@@ -1,15 +1,15 @@
 class Solution {
-    public int heightChecker(int[] heights) {
-        int[] sortedHeights = Arrays.copyOf(heights, heights.length);
-        Arrays.sort(sortedHeights);
-        int count = 0;
+    public int heightChecker(int[] heights) { // O(N)
+        int[] sortedHeights = Arrays.copyOf(heights, heights.length); // O(N)
+        Arrays.sort(sortedHeights); // O(N)
+        int count = 0; // O(1)
         
-        for (int i = 0; i < heights.length; i++) {
-            if (heights[i] != sortedHeights[i]) {
-                count++;
+        for (int i = 0; i < heights.length; i++) { // O(N)
+            if (heights[i] != sortedHeights[i]) { // O(1)
+                count++; // O(1)
             }
         }
         
-        return count;
+        return count; // O(1)
     }
 }
