8e2f3ece8f026b0164bfb21c0f0e41bf2709e15c
Kristina Head <1kristinahead@gmail.com>
2020-05-29 11:27:06 +0200

    Time complexity

--- find-pivot-index/Solution.java
+++ find-pivot-index/Solution.java
@@ -1,20 +1,20 @@
 class Solution {
-    public int pivotIndex(int[] nums) {
-        int sum = 0;
+    public int pivotIndex(int[] nums) { // O(N)
+        int sum = 0; // O(1)
         
-        for (int number : nums) {
-            sum += number; 
+        for (int number : nums) { // O(N)
+            sum += number; // O(1)
         }
         
-        int leftSum = 0;
+        int leftSum = 0; // O(1)
             
-        for (int i = 0; i < nums.length; i++) {
-            if (leftSum == sum - leftSum - nums[i]) {
-                return i;
+        for (int i = 0; i < nums.length; i++) { // O(N)
+            if (leftSum == sum - leftSum - nums[i]) { // O(1)
+                return i; // O(1)
             }
-            leftSum += nums[i];
+            leftSum += nums[i]; // O(1)
         }
         
-        return -1;
+        return -1; // O(1)
     }
 }
