7e6dba978d834b3205a0ef34400cacd7bad55bb9
Kristina Head <1kristinahead@gmail.com>
2020-06-10 14:37:44 +0200

    Time complexity

--- plus-one/Solution.java
+++ plus-one/Solution.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int[] plusOne(int[] digits) {
-        int carryOver = 1;
+    public int[] plusOne(int[] digits) { // O(N)
+        int carryOver = 1; // O(1)
         
-        for (int i = digits.length - 1; i >= 0; i--) {
-            digits[i] += carryOver;
-            if (digits[i] <= 9) {
-                return digits;
+        for (int i = digits.length - 1; i >= 0; i--) { // O(N)
+            digits[i] += carryOver; // O(1)
+            if (digits[i] <= 9) { // O(1)
+                return digits; // O(1)
             }
-            digits[i] = 0;
+            digits[i] = 0; // O(1)
         }
         
-        int[] newDigits = new int[digits.length + 1];
-        newDigits[0] = 1;
+        int[] newDigits = new int[digits.length + 1]; // O(N)
+        newDigits[0] = 1; // O(1)
         
-        return newDigits;
+        return newDigits; // O(1)
     }
 }
