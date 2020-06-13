3722237fb1252b19d2c61fa29ef663064048b0c5
Kristina Head <1kristinahead@gmail.com>
2020-06-13 14:08:59 +0200

    Time complexity analysis

--- diagonal-traverse/ImprovedSolution.java
+++ diagonal-traverse/ImprovedSolution.java
@@ -1,40 +1,40 @@
 class ImprovedSolution {
-    public int[] findDiagonalOrder(int[][] matrix) {
-        if (matrix == null || matrix.length == 0) {
-            return new int[0];
+    public int[] findDiagonalOrder(int[][] matrix) { // O(M * N)
+        if (matrix == null || matrix.length == 0) { // O(1)
+            return new int[0]; // O(1)
         }
         
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int[] diagonalOrder = new int[m * n];
-        int row = 0;
-        int col = 0;
+        int m = matrix.length; // O(1)
+        int n = matrix[0].length; // O(1)
+        int[] diagonalOrder = new int[m * n]; // O(M * N)
+        int row = 0; // O(1)
+        int col = 0; // O(1)
         
-        for (int i = 0; i < m * n; i++) {
-            diagonalOrder[i] = matrix[row][col];
+        for (int i = 0; i < m * n; i++) { // O(M * N)
+            diagonalOrder[i] = matrix[row][col]; // O(1)
             
-            if ((row + col) % 2 == 0) {
-                if (col == n - 1) { 
-                    row++; 
-                } else if (row == 0) { 
-                    col++; 
+            if ((row + col) % 2 == 0) { // O(1)
+                if (col == n - 1) { // O(1) 
+                    row++; // O(1)
+                } else if (row == 0) { // O(1)
+                    col++; // O(1)
                 } else { 
-                    row--;
-                    col++;
+                    row--; // O(1)
+                    col++; // O(1)
                 }
             } else {
-                if (row == m - 1) { 
-                    col++;
-                } else if (col == 0) { 
-                    row++;
+                if (row == m - 1) { // O(1)
+                    col++; // O(1)
+                } else if (col == 0) { // O(1)
+                    row++; // O(1)
                 } else { 
-                    row++;
-                    col--; 
+                    row++; // O(1)
+                    col--; // O(1)
                 }
             }   
         }
         
-        return diagonalOrder;
+        return diagonalOrder; // O(1)
     }
 }
    
