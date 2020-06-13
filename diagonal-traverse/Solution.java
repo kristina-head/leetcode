46db197a388e647f75541cea2c59606f16dc2b05
Kristina Head <1kristinahead@gmail.com>
2020-06-13 13:50:49 +0200

    Time complexity

--- diagonal-traverse/Solution.java
+++ diagonal-traverse/Solution.java
@@ -1,39 +1,39 @@
 class Solution {
-    public int[] findDiagonalOrder(int[][] matrix) {
-        if (matrix == null || matrix.length == 0) {
-            return new int[0];
+    public int[] findDiagonalOrder(int[][] matrix) { // O(M * N)
+        if (matrix == null || matrix.length == 0) { // O(1)
+            return new int[0]; // O(1)
         }
         
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int[] diagonalOrder = new int[m * n];
-        int index = 0;
-        int row = 0;
-        int col = 0;
-        int direction = 1;
+        int m = matrix.length; // O(1)
+        int n = matrix[0].length; // O(1)
+        int[] diagonalOrder = new int[m * n]; // O(M * N)
+        int index = 0; // O(1)
+        int row = 0; // O(1)
+        int col = 0; // O(1)
+        int direction = 1; // O(1)
         
-        while (row < m && col < n) {
-            diagonalOrder[index++] = matrix[row][col];
+        while (row < m && col < n) { // O(M * N)
+            diagonalOrder[index++] = matrix[row][col]; // O(1)
             
-            int newRow = row + (direction == 1 ? -1 : 1);
-            int newCol = col + (direction == 1 ? 1 : -1);
+            int newRow = row + (direction == 1 ? -1 : 1); // O(1)
+            int newCol = col + (direction == 1 ? 1 : -1); // O(1)
             
-            if (newRow < 0 || newRow == m || newCol < 0 || newCol == n) {
-                if (direction == 1) {
-                    row += (col == n - 1 ? 1 : 0);
-                    col += (col < n - 1 ? 1 : 0);
+            if (newRow < 0 || newRow == m || newCol < 0 || newCol == n) { // O(1)
+                if (direction == 1) { // O(1)
+                    row += (col == n - 1 ? 1 : 0); // O(1)
+                    col += (col < n - 1 ? 1 : 0); // O(1)
                 } else {
-                    col += (row == m - 1 ? 1 : 0);
-                    row += (row < m - 1 ? 1 : 0);
+                    col += (row == m - 1 ? 1 : 0); // O(1)
+                    row += (row < m - 1 ? 1 : 0); // O(1)
                 }
                 
-                direction = 1 - direction;
+                direction = 1 - direction; // O(1)
             } else {
-                row = newRow;
-                col = newCol;
+                row = newRow; // O(1)
+                col = newCol; // O(1)
             }
         }
         
-        return diagonalOrder;
+        return diagonalOrder; // O(1)
     }
 }
