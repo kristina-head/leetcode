ff793ef5e7cc5d0c345a58ef0f364d2d4a9824d7
Kristina Head <1kristinahead@gmail.com>
2020-09-19 22:29:40 +0200

    Solution with time complexity analysis

---
+++ shuffle-the-array/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) { // O(N*M)
+        int temp; // O(1)
+        
+        for (int i = 1; n < nums.length; i += 2) { // O(M)
+            temp = nums[n]; // O(1)
+            for (int j = n; j >= i; j--) { // O(N)
+                nums[j] = nums[j - 1]; // O(1)
+            }
+            nums[i] = temp; // O(1)
+            n++; // O(1)
+        }
+        
+        return nums; // O(1)
+    }
+}
