0c432e43189960d383488d4cd27ec37f4ec15e67
Kristina Head <1kristinahead@gmail.com>
2020-09-20 11:29:18 +0200

    Time complexity analysis

--- number-of-good-pairs/Solution.java
+++ number-of-good-pairs/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int numIdenticalPairs(int[] nums) {
-        Map<Integer, Integer> countPerNumber = new HashMap<>();
-        int identicalPairs = 0;
+    public int numIdenticalPairs(int[] nums) { // O(M+N)
+        Map<Integer, Integer> countPerNumber = new HashMap<>(); // O(1)
+        int identicalPairs = 0; // O(1)
         
-        for (int i = 0; i < nums.length; i++) {
-            int count = countPerNumber.containsKey(nums[i]) ? countPerNumber.get(nums[i]) : 0;
-            countPerNumber.put(nums[i], count + 1);
+        for (int i = 0; i < nums.length; i++) { // O(M)
+            int count = countPerNumber.containsKey(nums[i]) ? countPerNumber.get(nums[i]) : 0; // O(1)
+            countPerNumber.put(nums[i], count + 1); // O(1)
         }
         
-        for (int count : countPerNumber.values()) {
-            identicalPairs += count * (count - 1) / 2;
+        for (int count : countPerNumber.values()) { // O(N)
+            identicalPairs += count * (count - 1) / 2; // O(1)
         }
         
-        return identicalPairs;
+        return identicalPairs; // O(1)
     }
 }
