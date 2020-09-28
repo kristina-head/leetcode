22dbde2c0fb1afbd5879ab970f453eb3d1b89a8e
Kristina Head <1kristinahead@gmail.com>
2020-09-28 18:23:51 +0200

    Time complexity analysis

--- sum-of-all-odd-length-subarrays/Solution.java
+++ sum-of-all-odd-length-subarrays/Solution.java
@@ -1,12 +1,12 @@
 class Solution {
-    public int sumOddLengthSubarrays(int[] arr) { 
-        int total = 0; 
-        int length = arr.length; 
+    public int sumOddLengthSubarrays(int[] arr) { // O(N^3)
+        int total = 0; // O(1)
+        int length = arr.length; // O(1)
         
-        for (int i = 1; i <= length; i += 2) { 
-            for (int j = 0; j <= length - i; j++) { 
-                for (int k = 0; k < i; k++) { 
-                    total += arr[j + k]; 
+        for (int i = 1; i <= length; i += 2) { // O(N)
+            for (int j = 0; j <= length - i; j++) { // O(N)
+                for (int k = 0; k < i; k++) { // O(N)
+                    total += arr[j + k]; // O(1)
                 }
             }
         }
