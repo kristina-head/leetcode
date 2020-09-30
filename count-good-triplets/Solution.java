8ce225c17f4bad05a5f7a1225c379f05de7ffe92
Kristina Head <1kristinahead@gmail.com>
2020-09-30 18:42:12 +0200

    Time complexity analysis

--- count-good-triplets/Solution.java
+++ count-good-triplets/Solution.java
@@ -1,20 +1,20 @@
 class Solution {
-    public int countGoodTriplets(int[] arr, int a, int b, int c) {
-        int count = 0;
+    public int countGoodTriplets(int[] arr, int a, int b, int c) { // O(N^3)
+        int count = 0; // O(1)
         
-        for (int k = 2; k < arr.length; k++) {
-            for (int j = 1; j < k; j++) {
-                if (Math.abs(arr[j] - arr[k]) <= b) {
-                    for (int i = 0; i < j; i++) {
+        for (int k = 2; k < arr.length; k++) { // O(N)
+            for (int j = 1; j < k; j++) { // O(N)
+                if (Math.abs(arr[j] - arr[k]) <= b) { // O(1)
+                    for (int i = 0; i < j; i++) { // O(N)
                         if (Math.abs(arr[i] - arr[j]) <= a && 
-                            Math.abs(arr[i] - arr[k]) <= c) {
-                            count++;
+                            Math.abs(arr[i] - arr[k]) <= c) { // O(1)
+                            count++; // O(1)
                         }
                     }
                 }
             }
         }
         
-        return count;
+        return count; // O(1)
     }
 }
