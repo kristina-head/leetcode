6505637b91ecccce6a5f632f1b19a244edf9782c
Kristina Head <1kristinahead@gmail.com>
2020-10-03 12:31:26 +0200

    Time complexity analysis

--- matrix-diagonal-sum/Solution.java
+++ matrix-diagonal-sum/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int diagonalSum(int[][] mat) {
-        int length = mat.length;
-        int total = 0;
+    public int diagonalSum(int[][] mat) { // O(N)
+        int length = mat.length; // O(1)
+        int total = 0; // O(1)
         
-        for (int i = 0; i < length; i++) {
-            total += mat[i][i] + mat[i][length - 1 - i];
+        for (int i = 0; i < length; i++) { // O(N)
+            total += mat[i][i] + mat[i][length - 1 - i]; // O(1)
         }
         
-        if (length % 2 != 0) {
-            int middle = (length - 1) / 2;
-            total -= mat[middle][middle];
+        if (length % 2 != 0) { // O(1)
+            int middle = (length - 1) / 2; // O(1)
+            total -= mat[middle][middle]; // O(1)
         }
         
-        return total;
+        return total; // O(1)
     }
 }
