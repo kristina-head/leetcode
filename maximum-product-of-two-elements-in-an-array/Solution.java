3a71696667c8b1a9312ec0b9a2edadfe7c478915
Kristina Head <1kristinahead@gmail.com>
2020-10-04 16:47:32 +0200

    Time complexity

--- maximum-product-of-two-elements-in-an-array/Solution.java
+++ maximum-product-of-two-elements-in-an-array/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int maxProduct(int[] nums) {
-        int max = 0;
-        int previousMax = 0;
+    public int maxProduct(int[] nums) { // O(N)
+        int max = 0; // O(1)
+        int previousMax = 0; // O(1)
         
-        for (int i : nums) {
-            if (i >= max) {
-                previousMax = max;
-                max = i;
-            } else if (i >= previousMax) {
-                previousMax = i;
+        for (int i : nums) { // O(N)
+            if (i >= max) { // O(1)
+                previousMax = max; // O(1)
+                max = i; // O(1)
+            } else if (i >= previousMax) { // O(1)
+                previousMax = i; // O(1)
             }
         }
         
-        return (max-1)*(previousMax-1);
+        return (max-1)*(previousMax-1); // O(1)
     }
 }
