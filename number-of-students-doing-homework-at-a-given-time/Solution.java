84880e6f7be404ca957367090f3e5f6bd848c829
Kristina Head <1kristinahead@gmail.com>
2020-10-04 21:30:52 +0200

    Time complexity

--- number-of-students-doing-homework-at-a-given-time/Solution.java
+++ number-of-students-doing-homework-at-a-given-time/Solution.java
@@ -1,14 +1,14 @@
 class Solution {
-    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
-        int intervals = startTime.length;
-        int count = 0;
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) { // O(N)
+        int intervals = startTime.length; // O(1)
+        int count = 0; // O(1)
         
-        for (int i = 0; i < intervals; i++) {
-            if (queryTime >= startTime[i] && queryTime <= endTime[i]) {
-                count++;
+        for (int i = 0; i < intervals; i++) { // O(N)
+            if (queryTime >= startTime[i] && queryTime <= endTime[i]) { // O(1)
+                count++; // O(1)
             }
         }
         
-        return count;
+        return count; // O(1)
     }
 }
