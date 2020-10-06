92841d8472488f5c60e9247cb18bd38622b8c312
Kristina Head <1kristinahead@gmail.com>
2020-10-06 22:05:53 +0200

    Time complexity analysis

--- flipping-an-image/Solution.java
+++ flipping-an-image/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int[][] flipAndInvertImage(int[][] A) {
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < A.length / 2; j++) {
-                int temp = A[i][j];
-                A[i][j] = A[i][A.length - 1 - j];
-                A[i][A.length - 1 - j] = temp;
+    public int[][] flipAndInvertImage(int[][] A) { // O(N^2)
+        for (int i = 0; i < A.length; i++) { // O(N)
+            for (int j = 0; j < A.length / 2; j++) { // O(N/2)
+                int temp = A[i][j]; // O(1)
+                A[i][j] = A[i][A.length - 1 - j]; // O(1)
+                A[i][A.length - 1 - j] = temp; // O(1)
             }
             
-            for (int j = 0; j < A.length; j++) {
-                A[i][j] = A[i][j] == 0 ? 1 : 0;
+            for (int j = 0; j < A.length; j++) { // O(N)
+                A[i][j] = A[i][j] == 0 ? 1 : 0; // O(1)
             }
         }
         
-        return A;
+        return A; // O(1)
     }
 }
