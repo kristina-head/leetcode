af072f9d00b27ae8e73f9c5a3f57ae8b5f5df2f5
Kristina Head <1kristinahead@gmail.com>
2020-10-06 22:08:11 +0200

    Store A.length in a variable

--- flipping-an-image/Solution.java
+++ flipping-an-image/Solution.java
@@ -1,13 +1,15 @@
 class Solution {
     public int[][] flipAndInvertImage(int[][] A) { // O(N^2)
-        for (int i = 0; i < A.length; i++) { // O(N)
-            for (int j = 0; j < A.length / 2; j++) { // O(N/2)
+        int length = A.length;
+        
+        for (int i = 0; i < length; i++) { // O(N)
+            for (int j = 0; j < length / 2; j++) { // O(N/2)
                 int temp = A[i][j]; // O(1)
-                A[i][j] = A[i][A.length - 1 - j]; // O(1)
-                A[i][A.length - 1 - j] = temp; // O(1)
+                A[i][j] = A[i][length - 1 - j]; // O(1)
+                A[i][length - 1 - j] = temp; // O(1)
             }
             
-            for (int j = 0; j < A.length; j++) { // O(N)
+            for (int j = 0; j < length; j++) { // O(N)
                 A[i][j] = A[i][j] == 0 ? 1 : 0; // O(1)
             }
         }
