1a3fa204e98314d169268e1d10839e02256061f5
Kristina Head <1kristinahead@gmail.com>
2020-10-11 10:40:13 +0200

    Time complexity analysis

--- generate-a-string-with-characters-that-have-odd-counts/Solution.java
+++ generate-a-string-with-characters-that-have-odd-counts/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
-    public String generateTheString(int n) {
-        StringBuilder sb = new StringBuilder(n);
+    public String generateTheString(int n) { // O(N)
+        StringBuilder sb = new StringBuilder(n); // O(N)
         
-        for (int i = 0; i < n - 1; i++) {
-            sb.append('k');
+        for (int i = 0; i < n - 1; i++) { // O(N-1)
+            sb.append('k'); // O(1)
         }
         
-        if (n % 2 != 0) {
-            sb.append('k');
+        if (n % 2 != 0) { // O(1)
+            sb.append('k'); // O(1)
         } else {
-            sb.append('h');
+            sb.append('h'); // O(1)
         }
         
-        return sb.toString();
+        return sb.toString(); // O(N)
     }
 }
