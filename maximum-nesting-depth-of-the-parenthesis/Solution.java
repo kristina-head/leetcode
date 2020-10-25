447a64da6e26e9a1c92ec0022054fe90f04f0827
Kristina Head <1kristinahead@gmail.com>
2020-10-25 10:03:01 +0100

    Time complexity analysis

--- maximum-nesting-depth-of-the-parenthesis/Solution.java
+++ maximum-nesting-depth-of-the-parenthesis/Solution.java
@@ -1,19 +1,19 @@
 class Solution {
-    public int maxDepth(String s) {
-        int depth = 0;
-        int maxDepth = 0;
+    public int maxDepth(String s) { // O(N)
+        int depth = 0; // O(1)
+        int maxDepth = 0; // O(1)
         
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                depth++;
-            } else if (s.charAt(i) == ')') {
-                if (depth > maxDepth) {
-                    maxDepth = depth;
+        for (int i = 0; i < s.length(); i++) { // O(N)
+            if (s.charAt(i) == '(') { // O(1)
+                depth++; // O(1)
+            } else if (s.charAt(i) == ')') { // O(1)
+                if (depth > maxDepth) { // O(1)
+                    maxDepth = depth; // O(1)
                 }
-                depth--;
+                depth--; // O(1)
             }
         }
         
-        return maxDepth;
+        return maxDepth; // O(1)
     }
 }
