8e06c1018d620072818c0d857ddea15e6049913c
Kristina Head <1kristinahead@gmail.com>
2020-12-05 09:48:01 +0100

    Time complexity

--- merge-in-between-linked-lists/Solution.java
+++ merge-in-between-linked-lists/Solution.java
@@ -1,23 +1,23 @@
 class Solution {
-    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {  
-        ListNode end = getPosition(list1, 0, b+1);
-        getPosition(list1, 0, a-1).next = list2;
-        getLast(list2).next = end;
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) { // O(N)
+        ListNode end = getPosition(list1, 0, b+1); // O(N)
+        getPosition(list1, 0, a-1).next = list2; // O(N)
+        getLast(list2).next = end; // O(N)
         
-        return list1;
+        return list1; // O(1)
     }
     
-    private ListNode getPosition(ListNode listNode, int current, int position) {
-        if (current == position)
-            return listNode; 
+    private ListNode getPosition(ListNode listNode, int current, int position) { // O(N)
+        if (current == position) // O(1)
+            return listNode; // O(1)
         
-        return getPosition(listNode.next, current+1, position);
+        return getPosition(listNode.next, current+1, position); // O(N)
     }
     
-    private ListNode getLast(ListNode listNode) {
-        if (listNode.next == null)
-            return listNode;
+    private ListNode getLast(ListNode listNode) { // O(N)
+        if (listNode.next == null) // O(1)
+            return listNode; // O(1)
         
-        return getLast(listNode.next);
+        return getLast(listNode.next); // O(N)
     }
 }
