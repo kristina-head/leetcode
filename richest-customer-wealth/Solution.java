387a0a2a5135eb145c9ff76a332ddc1e39f7ce2e
Kristina Head <1kristinahead@gmail.com>
2020-12-06 12:12:28 +0100

    Time complexity analysis

--- richest-customer-wealth/Solution.java
+++ richest-customer-wealth/Solution.java
@@ -1,16 +1,16 @@
 class Solution {
-    public int maximumWealth(int[][] accounts) {
-        int wealth; 
-        int maxWealth = 0;
+    public int maximumWealth(int[][] accounts) { // O(N*M)
+        int wealth; // O(1)
+        int maxWealth = 0; // O(1)
         
-        for (int i = 0; i < accounts.length; i++) {
-            wealth = 0;
-            for (int j = 0; j < accounts[i].length; j++) {
-                wealth += accounts[i][j];
+        for (int i = 0; i < accounts.length; i++) { // O(M)
+            wealth = 0; // O(1)
+            for (int j = 0; j < accounts[i].length; j++) { // O(N)
+                wealth += accounts[i][j]; // O(1)
             }
-            maxWealth = Math.max(maxWealth, wealth);
+            maxWealth = Math.max(maxWealth, wealth); // O(1)
         }
         
-        return maxWealth;
+        return maxWealth; // O(1)
     }
 }
