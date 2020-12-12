d332451f31d52a1eaf280c3d2813fcb30dd68de4
Kristina Head <1kristinahead@gmail.com>
2020-12-12 17:19:30 +0100

    Time complexity
    Internally, a `StringBuilder` is a Char Array. `.append()` will iterate over the String passed as an argument and add each Char to the end of the Array. Therefore, calling `.append("al")` will be O(2), but we simplify this to O(1). Since big O is also not concerned with non-dominant terms, we can drop the O(M) from calling `sb.toString()`, which will leave us with a linear running time

--- goal-parser-interpretation/Solution.java
+++ goal-parser-interpretation/Solution.java
@@ -1,19 +1,19 @@
 class Solution {
-    public String interpret(String command) {
-        StringBuilder sb = new StringBuilder();
+    public String interpret(String command) { // O(N)
+        StringBuilder sb = new StringBuilder(); // O(1)
         
-        for (int i = 0; i < command.length(); i++) {
-            if (command.charAt(i) == 'G') {
-                sb.append('G');
-            } else if (command.charAt(i) == '(') {
-                if (command.charAt(i+1) == ')') {
-                    sb.append('o');
+        for (int i = 0; i < command.length(); i++) { // O(N)
+            if (command.charAt(i) == 'G') { // O(1)
+                sb.append('G'); // O(1)
+            } else if (command.charAt(i) == '(') { // O(1)
+                if (command.charAt(i+1) == ')') { // O(1)
+                    sb.append('o'); // O(1)
                 } else {
-                    sb.append("al");
+                    sb.append("al"); // O(1)
                 }
             }
         }
         
-        return sb.toString();
+        return sb.toString(); // O(M)
     }
 }
