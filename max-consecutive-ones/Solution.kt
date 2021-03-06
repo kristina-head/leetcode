f470d2a13cb10fef75824a0323f6512e33de61ed
Kristina Head <1kristinahead@gmail.com>
2021-03-06 14:01:51 +0100

    Time complexity

--- max-consecutive-ones/Solution.kt
+++ max-consecutive-ones/Solution.kt
@@ -1,19 +1,19 @@
 class Solution {
-    fun findMaxConsecutiveOnes(nums: IntArray): Int {
-        var max = 0
-        var count = 0
+    fun findMaxConsecutiveOnes(nums: IntArray): Int { // O(N)
+        var max = 0 // O(1)
+        var count = 0 // O(1)
         
-        for (i in nums.indices) {
-            if (nums[i] == 1) {
-                count++
-                if (count > max) {
-                    max = count
+        for (i in nums.indices) { // O(N)
+            if (nums[i] == 1) { // O(1)
+                count++ // O(1)
+                if (count > max) { // O(1)
+                    max = count // O(1)
                 }
-            } else {
-                count = 0
+            } else { 
+                count = 0 // O(1)
             }
         }
         
-        return max
+        return max // O(1)
     }
 }
