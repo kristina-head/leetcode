b5d782432c5794dd594fccb1ce70876cdd14b210
Kristina Head <1kristinahead@gmail.com>
2021-03-20 08:38:45 +0100

    Time complexity analysis

--- merge-sorted-array/Solution.kt
+++ merge-sorted-array/Solution.kt
@@ -1,14 +1,14 @@
 class Solution {
-    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
-        var i = m - 1
-        var j = n - 1
-        var k = m + n - 1
+    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit { // O(N)
+        var i = m - 1 // O(1)
+        var j = n - 1 // O(1)
+        var k = m + n - 1 // O(1)
         
-        while (k >= 0) {
-            if (i < 0 || j >= 0 && nums1[i] < nums2[j]) {
-                nums1[k--] = nums2[j--]
+        while (k >= 0) { // O(N)
+            if (i < 0 || j >= 0 && nums1[i] < nums2[j]) { // O(1)
+                nums1[k--] = nums2[j--] // O(1)
             } else {
-                nums1[k--] = nums1[i--]
+                nums1[k--] = nums1[i--] // O(1)
             }
         }
     }
