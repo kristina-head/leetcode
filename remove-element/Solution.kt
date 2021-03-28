3ef6483e910ab0ef7aefa92c1ddd8d13c8b755cc
Kristina Head <1kristinahead@gmail.com>
2021-03-28 20:02:14 +0200

    Time complexity

--- remove-element/Solution.kt
+++ remove-element/Solution.kt
@@ -1,9 +1,9 @@
 class Solution {
-    fun removeElement(nums: IntArray, `val`: Int): Int {
-        var size = 0
+    fun removeElement(nums: IntArray, `val`: Int): Int { // O(N)
+        var size = 0 // O(1)
 
-        nums.forEach { if (it != `val`) nums[size++] = it }
+        nums.forEach { if (it != `val`) nums[size++] = it } // O(N)
 
-        return size
+        return size // O(1)
     }
 }
