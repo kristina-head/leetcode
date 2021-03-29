2745614460713b4787ef94f58edad0a08e84d69a
Kristina Head <1kristinahead@gmail.com>
2021-03-29 13:51:51 +0200

    Time complexity

--- remove-duplicates-from-sorted-array/Solution.kt
+++ remove-duplicates-from-sorted-array/Solution.kt
@@ -1,13 +1,13 @@
 class Solution {
-    fun removeDuplicates(nums: IntArray): Int {
-        var i = 0
+    fun removeDuplicates(nums: IntArray): Int { // O(N)
+        var i = 0 // O(1) 
         
-        for (j in nums.indices) {
-            if (nums[j] != nums[i]) {
-                nums[++i] = nums[j]
+        for (j in nums.indices) { // O(N)
+            if (nums[j] != nums[i]) { // O(1)
+                nums[++i] = nums[j] // O(1)
             }
         }
         
-        return i + 1;
+        return i + 1 // O(1)
     }
 }
