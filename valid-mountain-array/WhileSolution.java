6d380e1aba30c7a59b3c5158dbffc874d4117d97
Kristina Head <1kristinahead@gmail.com>
2021-04-05 13:30:51 +0200

    Time complexity

--- valid-mountain-array/WhileSolution.java
+++ valid-mountain-array/WhileSolution.java
@@ -1,18 +1,18 @@
 class WhileSolution {
-    public boolean validMountainArray(int[] arr) {
-        int n = arr.length;
-        int i = 0;
+    public boolean validMountainArray(int[] arr) { // O(N)
+        int n = arr.length; // O(1)
+        int i = 0; // O(1)
 
-        while (i + 1 < n && arr[i] < arr[i + 1])
-            i++;
+        while (i + 1 < n && arr[i] < arr[i + 1]) // O(N)
+            i++; // O(1)
 
-        if (i == 0 || i == n - 1)
-            return false;
+        if (i == 0 || i == n - 1) // O(1)
+            return false; // O(1)
 
-        while (i + 1 < n && arr[i] > arr[i + 1])
-            i++;
+        while (i + 1 < n && arr[i] > arr[i + 1]) // O(N)
+            i++; // O(1)
 
-        return i == n - 1;
+        return i == n - 1; // O(1)
     }
 }
 
