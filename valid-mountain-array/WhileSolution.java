b65f83c7e23bb15a44aa1d74f4c52987687a2e13
Kristina Head <1kristinahead@gmail.com>
2021-04-05 13:29:31 +0200

    Java while loop solution

---
+++ valid-mountain-array/WhileSolution.java
@@ -0,0 +1,18 @@
+class WhileSolution {
+    public boolean validMountainArray(int[] arr) {
+        int n = arr.length;
+        int i = 0;
+
+        while (i + 1 < n && arr[i] < arr[i + 1])
+            i++;
+
+        if (i == 0 || i == n - 1)
+            return false;
+
+        while (i + 1 < n && arr[i] > arr[i + 1])
+            i++;
+
+        return i == n - 1;
+    }
+}
+
