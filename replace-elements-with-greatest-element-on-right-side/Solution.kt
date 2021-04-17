a3c92137f3a6efa2f2acc6f5843e9b509770f716
Kristina Head <1kristinahead@gmail.com>
2021-04-17 14:40:13 +0200

    Time complexity analysis

--- replace-elements-with-greatest-element-on-right-side/Solution.kt
+++ replace-elements-with-greatest-element-on-right-side/Solution.kt
@@ -1,13 +1,13 @@
 class Solution {
-    fun replaceElements(arr: IntArray): IntArray {
-        var max = -1
+    fun replaceElements(arr: IntArray): IntArray { // O(N)
+        var max = -1 // O(1)
         
-        for (i in arr.size-1 downTo 0) {
-            val current = max
-            max = Math.max(max, arr[i])
-            arr[i] = current
+        for (i in arr.size-1 downTo 0) { // O(N)
+            val current = max // O(1)
+            max = Math.max(max, arr[i]) // O(1)
+            arr[i] = current // O(1)
         }
         
-        return arr
+        return arr // O(1)
     }
 }
