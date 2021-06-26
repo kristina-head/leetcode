87e6fd4db1b847fba654dbf24e029e51d9c44a81
Kristina Head <1kristinahead@gmail.com>
2021-06-26 20:00:29 +0200

    Time complexity analysis

--- largest-number-at-least-twice-of-others/Solution.kt
+++ largest-number-at-least-twice-of-others/Solution.kt
@@ -1,19 +1,19 @@
 class Solution {
-    fun dominantIndex(nums: IntArray): Int {
-        var max = nums[0]
-        var maxIndex = 0
+    fun dominantIndex(nums: IntArray): Int { // O(N)
+        var max = nums[0] // O(1)
+        var maxIndex = 0 // O(1)
         
-        for (i in nums.indices) {
-            if (nums[i] > max) {
-                max = nums[i]
-                maxIndex = i
+        for (i in nums.indices) { // O(N)
+            if (nums[i] > max) { // O(1)
+                max = nums[i] // O(1)
+                maxIndex = i // O(1)
             }
         }
         
-        for (i in nums.indices) {
-            if (i != maxIndex && max < nums[i] * 2) return -1
+        for (i in nums.indices) { // O(N)
+            if (i != maxIndex && max < nums[i] * 2) return -1 // O(1)
         }
         
-        return maxIndex
+        return maxIndex // O(1)
     }
 }
