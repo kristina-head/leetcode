5c3842d8275d1c8fbacc0b05acb3e58c4e1c5045
Kristina Head <1kristinahead@gmail.com>
2021-06-27 15:56:15 +0200

    Time complexity

--- plus-one/WhileLoopSolution.java
+++ plus-one/WhileLoopSolution.java
@@ -1,19 +1,19 @@
 class WhileLoopSolution {
-    public int[] plusOne(int[] digits) {
-        int i = digits.length-1;
+    public int[] plusOne(int[] digits) { // O(N)
+        int i = digits.length-1; // O(1)
 
-        while (i >= 0 && digits[i] == 9) {
-            digits[i] = 0;
-            i--;
+        while (i >= 0 && digits[i] == 9) { // O(N)
+            digits[i] = 0; // O(1)
+            i--; // O(1)
         }
         
-        if (i == -1) {
-            digits = new int[digits.length+1];
-            digits[0] = 1;
+        if (i == -1) { // O(1)
+            digits = new int[digits.length+1]; // O(N)
+            digits[0] = 1; // O(1)
         } else {
-            digits[i] += 1;   
+            digits[i] += 1; // O(1)
         }
         
-        return digits;
+        return digits; // O(1)
     }
 }
