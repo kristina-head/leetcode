bd8be8f754bfbf7fff8b1502030a3294d00ce881
Kristina Head <1kristinahead@gmail.com>
2022-09-11 11:04:14 +0200

    Time complexity

--- build-array-from-permutation/solution.java
+++ build-array-from-permutation/solution.java
@@ -1,11 +1,11 @@
 class Solution {
-    public int[] buildArray(int[] nums) {
-        int[] ans = new int[nums.length];
+    public int[] buildArray(int[] nums) { // O(N)
+        int[] ans = new int[nums.length]; // O(N)
         
-        for (int i = 0; i < nums.length; i++) {
-            ans[i] = nums[nums[i]];
+        for (int i = 0; i < nums.length; i++) { // O(N)
+            ans[i] = nums[nums[i]]; // O(1)
         }
         
-        return ans;
+        return ans; // O(1)
     }
 }
