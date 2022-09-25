e556fccc394dd57a49e5d6c46693b2af57ba3e65
Kristina Head <1kristinahead@gmail.com>
2022-09-25 11:22:41 +0200

    Time complexity

--- final-value-of-variable-after-performing-operations/solution.java
+++ final-value-of-variable-after-performing-operations/solution.java
@@ -1,15 +1,15 @@
 class Solution {
-    public int finalValueAfterOperations(String[] operations) {
-        int x = 0;
+    public int finalValueAfterOperations(String[] operations) { // O(N)
+        int x = 0; // O(1)
         
-        for (String operation : operations) {
-            if (operation.charAt(1) == '+') {
-                x++;
+        for (String operation : operations) { // O(N)
+            if (operation.charAt(1) == '+') { // O(1)
+                x++; // O(1)
             } else {
-                x--;
+                x--; // O(1)
             }
         }
         
-        return x;
+        return x; // O(1)
     }
 }
