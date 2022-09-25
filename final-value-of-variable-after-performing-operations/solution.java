f2f3fc19ee7887755a36a8a33a7ada3c2bce000e
Kristina Head <1kristinahead@gmail.com>
2022-09-25 11:21:22 +0200

    Solution

---
+++ final-value-of-variable-after-performing-operations/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        
+        for (String operation : operations) {
+            if (operation.charAt(1) == '+') {
+                x++;
+            } else {
+                x--;
+            }
+        }
+        
+        return x;
+    }
+}
