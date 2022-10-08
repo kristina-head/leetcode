339fe84b0eb89796609450c091875d430ff3e458
Kristina Head <1kristinahead@gmail.com>
2022-10-08 14:59:32 +0200

    Time complexity analysis

--- minimum-sum-of-four-digit-number-after-splitting-digits/solution.java
+++ minimum-sum-of-four-digit-number-after-splitting-digits/solution.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int minimumSum(int num) {
-        int[] nums = new int[4];
-        int i = 0;
+    public int minimumSum(int num) { // O(N log N)
+        int[] nums = new int[4]; // O(N)
+        int i = 0; // O(1)
         
-        while (num > 0) {
-            nums[i++] = num % 10;
-            num /= 10;
+        while (num > 0) { // O(N)
+            nums[i++] = num % 10; // O(1)
+            num /= 10; // O(1)
         }
         
-        Arrays.sort(nums);
-        int new1 = nums[0] * 10 + nums[2];
-        int new2 = nums[1] * 10 + nums[3];
+        Arrays.sort(nums); // O(N log N)
+        int new1 = nums[0] * 10 + nums[2]; // O(1)
+        int new2 = nums[1] * 10 + nums[3]; // O(1)
         
-        return new1 + new2;
+        return new1 + new2; // O(1)
     }
 }
