2013780c26cfb9cf8fb8668151dc5f64cdef8c8b
Kristina Head <1kristinahead@gmail.com>
2022-11-06 11:31:56 +0100

    Add time complexity analysis

--- smallest-even-multiple/solution.java
+++ smallest-even-multiple/solution.java
@@ -1,7 +1,7 @@
 class Solution {
-    public int smallestEvenMultiple(int n) {    
-        int multiple = n * 2;
+    public int smallestEvenMultiple(int n) { // O(1)
+        int multiple = n * 2; // O(1)
         
-        return (n % 2 == 0) ? multiple / 2 : multiple;
+        return (n % 2 == 0) ? multiple / 2 : multiple; // O(1)
     }
 }
