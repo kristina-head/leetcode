eae89f8b7feaa9a299b0e881077d4dede03c7791
Kristina Head <1kristinahead@gmail.com>
2022-11-13 10:49:40 +0100

    Time complexity

--- cells-in-a-range-on-an-excel-sheet/solution.java
+++ cells-in-a-range-on-an-excel-sheet/solution.java
@@ -1,14 +1,14 @@
 class Solution {
-    public List<String> cellsInRange(String s) {
-        List<String> cells = new ArrayList<String>();
+    public List<String> cellsInRange(String s) { // O(N*M)
+        List<String> cells = new ArrayList<String>(); // O(1)
         
-        for (char i = s.charAt(0); i <= s.charAt(3); i++) {
-            for (char j = s.charAt(1); j <= s.charAt(4); j++ ) {
-                String cell = Character.toString(i) + Character.toString(j);
-                cells.add(cell);
+        for (char i = s.charAt(0); i <= s.charAt(3); i++) { // O(N*M)
+            for (char j = s.charAt(1); j <= s.charAt(4); j++ ) { // O(M)
+                String cell = Character.toString(i) + Character.toString(j); // O(1)
+                cells.add(cell); // O(1)
             }
         }
         
-        return cells;
+        return cells; // O(1)
     }
 }
