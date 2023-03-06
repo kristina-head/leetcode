ab18e5e940d435f006d7a2218bfe0455726165cb
Kristina Head <1kristinahead@gmail.com>
2023-03-06 18:25:22 +0100

    Time complexity analysis

--- two-sum/MapSolution.java
+++ two-sum/MapSolution.java
@@ -1,17 +1,17 @@
 class MapSolution {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> indexMap = new HashMap<>(nums.length);
+    public int[] twoSum(int[] nums, int target) { // O(N)
+        Map<Integer, Integer> indexMap = new HashMap<>(nums.length); // O(1)
 
-        for (int i = 0; i < nums.length; i++) {
-            int counterpart = target - nums[i];
+        for (int i = 0; i < nums.length; i++) { // O(N)
+            int counterpart = target - nums[i]; // O(1)
             
-            if (indexMap.containsKey(counterpart)) {
-                return new int[] {i, indexMap.get(counterpart)};
+            if (indexMap.containsKey(counterpart)) { // O(1)
+                return new int[] {i, indexMap.get(counterpart)}; // O(1)
             }
 
-            indexMap.put(nums[i], i);
+            indexMap.put(nums[i], i); // O(1)
         }
 
-        return null;
+        return null; // O(1)
     }
 }
