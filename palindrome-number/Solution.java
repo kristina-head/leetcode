83b1fa9c6591f6914b85d0d0ba2932873b3b46c8
Kristina Head <1kristinahead@gmail.com>
2023-03-11 17:12:33 +0100

    Add time complexity

--- palindrome-number/Solution.java
+++ palindrome-number/Solution.java
@@ -1,13 +1,13 @@
 class Solution {
-    public boolean isPalindrome(int x) {
-        int num = x;
-        int reversedNum = 0;
+    public boolean isPalindrome(int x) { // O(N) where N is the number of digits in x
+        int num = x; // O(1)
+        int reversedNum = 0; // O(1)
 
-        while (x > 0) {
-            reversedNum = reversedNum * 10 + x % 10;
-            x /= 10;
+        while (x > 0) { // O(N)
+            reversedNum = reversedNum * 10 + x % 10; // O(1)
+            x /= 10; // O(1)
         }
 
-        return num == reversedNum;
+        return num == reversedNum; // O(1)
     }
 }
