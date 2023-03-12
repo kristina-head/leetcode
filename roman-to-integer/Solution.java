af61e7d8dc27047a5bdf092eee3538a01629f555
Kristina Head <1kristinahead@gmail.com>
2023-03-12 14:20:23 +0100

    Time complexity analysis

--- roman-to-integer/Solution.java
+++ roman-to-integer/Solution.java
@@ -1,25 +1,25 @@
 class Solution {
-    public int romanToInt(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
+    public int romanToInt(String s) { // O(N) where N is the number of chars in s
+        Map<Character, Integer> map = new HashMap<>(); // O(1)
+        map.put('I', 1); // O(1)
+        map.put('V', 5); // O(1)
+        map.put('X', 10); // O(1)
+        map.put('L', 50); // O(1)
+        map.put('C', 100); // O(1)
+        map.put('D', 500); // O(1)
+        map.put('M', 1000); // O(1)
 
-        int romanInt = 0;
+        int romanInt = 0; // O(1)
 
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (i > 0 && map.get(s.charAt(i)) > map.get(s.charAt(i - 1))) {
-                romanInt += map.get(s.charAt(i)) - map.get(s.charAt(i - 1));
-                i--;
+        for (int i = s.length() - 1; i >= 0; i--) { // O(N)
+            if (i > 0 && map.get(s.charAt(i)) > map.get(s.charAt(i - 1))) { // O(1)
+                romanInt += map.get(s.charAt(i)) - map.get(s.charAt(i - 1)); // O(1)
+                i--; // O(1)
             } else {
-                romanInt += map.get(s.charAt(i));
+                romanInt += map.get(s.charAt(i)); // O(1)
             }
         }
 
-        return romanInt;
+        return romanInt; // O(1)
     }
 }
