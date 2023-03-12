985c715230b109c007b6edf6a8854367fd58063a
Kristina Head <1kristinahead@gmail.com>
2023-03-12 14:17:25 +0100

    Solution

---
+++ roman-to-integer/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int romanInt = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (i > 0 && map.get(s.charAt(i)) > map.get(s.charAt(i - 1))) {
+                romanInt += map.get(s.charAt(i)) - map.get(s.charAt(i - 1));
+                i--;
+            } else {
+                romanInt += map.get(s.charAt(i));
+            }
+        }
+
+        return romanInt;
+    }
+}
