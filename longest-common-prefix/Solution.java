ca91f6d2562dc9c7c166a9344dd8828dd2287fa4
Kristina Head <1kristinahead@gmail.com>
2023-03-18 11:42:40 +0100

    Add time complexity analysis

--- longest-common-prefix/Solution.java
+++ longest-common-prefix/Solution.java
@@ -1,12 +1,12 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) {     
-        for (int i = 0; i < strs[0].length(); i++) {
-            char current = strs[0].charAt(i);
-            for (int j = 1; j < strs.length; j++) {
-                if (i == strs[j].length() || strs[j].charAt(i) != current) return strs[0].substring(0, i);
+    public String longestCommonPrefix(String[] strs) { // O(M * N) where M is the no. of chars in strs[0] and N is the no. of Strings in strs    
+        for (int i = 0; i < strs[0].length(); i++) { // O(M)
+            char current = strs[0].charAt(i); // O(1)
+            for (int j = 1; j < strs.length; j++) { // O(N)
+                if (i == strs[j].length() || strs[j].charAt(i) != current) return strs[0].substring(0, i); // O(M)
             }
         }
 
-        return strs[0];
+        return strs[0]; // O(1)
     }
 }
