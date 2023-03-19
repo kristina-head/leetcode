07305cfa6b25d7245df9f923f2874d230734b3c3
Kristina Head <1kristinahead@gmail.com>
2023-03-19 19:47:48 +0100

    Time complexity

--- count-items-matching-a-rule/Solution.java
+++ count-items-matching-a-rule/Solution.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-        int count = 0;
-        int i = switch (ruleKey) {
-            case "type" -> 0;
-            case "color" -> 1;
-            case "name" -> 2;
-            default -> -1;
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) { // O(N * M) where N is the number of List<String> item in items and M is the number of chars in item.get(i)/ruleValue
+        int count = 0; // O(1)
+        int i = switch (ruleKey) { // O(1)
+            case "type" -> 0; // O(1)
+            case "color" -> 1; // O(1)
+            case "name" -> 2; // O(1)
+            default -> -1; // O(1)
         };
-        if (i < 0) return count;
+        if (i < 0) return count; // O(1)
 
-        for (List<String> item : items) {
-            if (item.get(i).equals(ruleValue)) count++;
+        for (List<String> item : items) { // O(N)
+            if (item.get(i).equals(ruleValue)) count++; // O(M)
         }
 
-        return count;
+        return count; // O(1)
     }
 }
