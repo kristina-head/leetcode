ce9b304d0b70a078b25bcf75af5701d318b537d6
Kristina Head <1kristinahead@gmail.com>
2023-03-26 16:01:07 +0200

    Time complexity analysis

--- linked-list-cycle/Solution.java
+++ linked-list-cycle/Solution.java
@@ -10,17 +10,17 @@
  * }
  */
 public class Solution {
-    public boolean hasCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
+    public boolean hasCycle(ListNode head) { // O(N) where N is the no. of ListNodes in the Linked List
+        ListNode slow = head; // O(1)
+        ListNode fast = head; // O(1)
         
-        while (fast != null && fast.next != null) {            
-            slow = slow.next;
-            fast = fast.next.next;
+        while (fast != null && fast.next != null) { // O(N)
+            slow = slow.next; // O(1)
+            fast = fast.next.next; // O(1)
             
-            if (slow == fast) return true;
+            if (slow == fast) return true; // O(1)
         }
         
-        return false;
+        return false; // O(1)
     }
 }
