c3311facf61b3e9b548b3958e43029dfb778fe44
Kristina Head <1kristinahead@gmail.com>
2023-04-01 16:55:42 +0200

    Add time complexity analysis

--- left-and-right-sum-differences/Solution.java
+++ left-and-right-sum-differences/Solution.java
@@ -1,30 +1,30 @@
 class Solution {
-    public int[] leftRigthDifference(int[] nums) {
-        int length = nums.length;
-        int count = 0;
-        int[] left = new int[length];
-        int[] right = new int[length];
-        int[] answer = new int[length];
+    public int[] leftRigthDifference(int[] nums) { // O(N) where N is the number of elements in nums
+        int length = nums.length; // O(1)
+        int count = 0; // O(1)
+        int[] left = new int[length]; // O(1)
+        int[] right = new int[length]; // O(1)
+        int[] answer = new int[length]; // O(1)
 
-        for (int i = 0; i < length - 1; i++) {
-            count += nums[i];
-            left[i + 1] = count;
+        for (int i = 0; i < length - 1; i++) { // O(N)
+            count += nums[i]; // O(1)
+            left[i + 1] = count; // O(1)
         }
 
-        count = 0;
+        count = 0; // O(1)
 
-        for (int i = length - 1; i > 0; i--) {
-            count += nums[i];
-            right[i - 1] = count;
+        for (int i = length - 1; i > 0; i--) { // O(N)
+            count += nums[i]; // O(1)
+            right[i - 1] = count; // O(1)
         }
 
-        for (int i = 0; i < length; i++) {
-            if (i == 0) answer[i] = right[i];
-            if (i == length - 1) answer[i] = left[i];
+        for (int i = 0; i < length; i++) { // O(N)
+            if (i == 0) answer[i] = right[i]; // O(1)
+            if (i == length - 1) answer[i] = left[i]; // O(1)
 
-            answer[i] = Math.abs(left[i] - right[i]);
+            answer[i] = Math.abs(left[i] - right[i]); // O(1)
         }
 
-        return answer;
+        return answer; // O(1)
     }
 }
