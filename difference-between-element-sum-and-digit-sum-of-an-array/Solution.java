7d3dfd19e4deb0d403f7cbb77d74f8c256bcf5a8
Kristina Head <1kristinahead@gmail.com>
2023-04-07 19:46:21 +0200

    Add time complexity

--- difference-between-element-sum-and-digit-sum-of-an-array/Solution.java
+++ difference-between-element-sum-and-digit-sum-of-an-array/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int differenceOfSum(int[] nums) {
-        int elementSum = 0;
-        int digitSum = 0;
+    public int differenceOfSum(int[] nums) { // O(N*M) where N is number of elements in nums and M is the number of digits in each element
+        int elementSum = 0; // O(1)
+        int digitSum = 0; // O(1)
 
-        for (int i = 0; i < nums.length; i++) {
-            elementSum += nums[i];
+        for (int i = 0; i < nums.length; i++) { // O(N)
+            elementSum += nums[i]; // O(1)
 
-            while (nums[i] > 0) {
-                digitSum += nums[i] % 10;
-                nums[i] /= 10;
+            while (nums[i] > 0) { // O(M)
+                digitSum += nums[i] % 10; // O(1)
+                nums[i] /= 10; // O(1)
             }
         }
 
-        return Math.abs(elementSum - digitSum);
+        return Math.abs(elementSum - digitSum); // O(1)
     }
 }
