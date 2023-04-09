298ce19cc92a3df5e3bc53fc9ea4c93496fd424b
Kristina Head <1kristinahead@gmail.com>
2023-04-09 14:18:16 +0200

    Add time complexity

--- sorting-the-sentence/Solution.java
+++ sorting-the-sentence/Solution.java
@@ -1,14 +1,14 @@
 class Solution {
-    public String sortSentence(String s) {
-        String[] words = s.split(" ");
-        String[] result = new String[words.length];
+    public String sortSentence(String s) { // O(N + M) where N is s.length() and M is words.length()
+        String[] words = s.split(" "); // O(N)
+        String[] result = new String[words.length]; // O(1)
 
-        for (String w : words) {
-            int lastCharIndex = w.length() - 1;
-            int pos = Character.getNumericValue(w.charAt(lastCharIndex)) - 1;
-            result[pos] = w.substring(0, lastCharIndex);
+        for (String w : words) { // O(M)
+            int lastCharIndex = w.length() - 1; // O(1)
+            int pos = Character.getNumericValue(w.charAt(lastCharIndex)) - 1; // O(1)
+            result[pos] = w.substring(0, lastCharIndex); // O(1)
         }
 
-        return String.join(" ", result);
+        return String.join(" ", result); // O(M)
     }
 }
