40bcf27faf893054de8bb7c6892ccf004ac296ee
Kristina Head <1kristinahead@gmail.com>
2023-04-15 12:24:52 +0200

    Add time complexity analysis

--- remove-linked-list-elements/Solution.java
+++ remove-linked-list-elements/Solution.java
@@ -9,26 +9,26 @@
  * }
  */
 class Solution {
-    public ListNode removeElements(ListNode head, int val) {            
-        while (head != null && head.val == val) {
-            head = head.next;
+    public ListNode removeElements(ListNode head, int val) { // O(N) where N is the number of nodes in the list
+        while (head != null && head.val == val) { // O(N)
+            head = head.next; // O(1)
         }
         
-        if (head == null) return head;
+        if (head == null) return head; // O(1)
         
-        ListNode prev = head; 
-        ListNode node = head.next;
+        ListNode prev = head; // O(1)
+        ListNode node = head.next; // O(1)
         
-        while (node != null) {
-            if (node.val == val) {
-                prev.next = node.next;
+        while (node != null) { // O(N)
+            if (node.val == val) { // O(1)
+                prev.next = node.next; // O(1)
             } else {
-                prev = node;
+                prev = node; // O(1)
             }
             
-            node = node.next;
+            node = node.next; // O(1)
         }
         
-        return head;
+        return head; // O(1)
     }
 }
