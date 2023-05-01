2cef8f6c3078460cb7b859b7273f6b31a032ba49
Kristina Head <1kristinahead@gmail.com>
2023-05-01 08:35:09 +0200

    Time complexity

--- reverse-string/Solution.java
+++ reverse-string/Solution.java
@@ -1,12 +1,12 @@
 class Solution {
-    public void reverseString(char[] s) {
-        int i = 0;
-        int j = s.length - 1;
+    public void reverseString(char[] s) { // O(N)
+        int i = 0; // O(1)
+        int j = s.length - 1; // O(1)
         
-        while (i < j) {
-            char temp = s[i];
-            s[i++] = s[j];
-            s[j--] = temp;
+        while (i < j) { // O(N)
+            char temp = s[i]; // O(1)
+            s[i++] = s[j]; // O(1)
+            s[j--] = temp; // O(1)
         }
     }
 }
