ccf1e46ddc87b51b71d9d48f8f603e66c7bd809c
Kristina Head <1kristinahead@gmail.com>
2023-05-07 17:31:43 +0200

    Add time complexity for methods

--- min-stack/MinStack.java
+++ min-stack/MinStack.java
@@ -2,18 +2,18 @@ class MinStack {
     List<Integer> stack;
     List<Integer> minStack;
 
-    public MinStack() {
+    public MinStack() { // O(1)
         this.stack = new ArrayList<>();
         this.minStack = new ArrayList<>();
     }
     
-    public void push(int val) {      
+    public void push(int val) { // O(1)      
         if (stack.isEmpty() || val <= minStack.get(minStack.size() - 1)) minStack.add(val);
         
         stack.add(val);
     }
     
-    public void pop() {
+    public void pop() { // O(1)
         int element = stack.get(stack.size() - 1);
         int min = minStack.get(minStack.size() - 1);
         
@@ -22,11 +22,11 @@ class MinStack {
         stack.remove(stack.size() - 1);
     }
     
-    public int top() {
+    public int top() { // O(1)
         return stack.get(stack.size() - 1);
     }
     
-    public int getMin() {
+    public int getMin() { // O(1)
         return minStack.get(minStack.size() - 1);
     }
 }
