39b7f0386101e4812373a5f9ae5c60b001de9bad
Kristina Head <1kristinahead@gmail.com>
2023-05-08 13:50:18 +0200

    Add time complexity analysis

--- valid-parentheses/Solution.java
+++ valid-parentheses/Solution.java
@@ -1,25 +1,25 @@
 class Solution {
-    public boolean isValid(String s) {
-        if (s.length() % 2 != 0) return false;
+    public boolean isValid(String s) { // O(N) where N is the number of chars in s
+        if (s.length() % 2 != 0) return false; // O(1)
             
-        Deque<Character> stack = new ArrayDeque<>();
+        Deque<Character> stack = new ArrayDeque<>(); // O(1)
         
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
+        for (char c : s.toCharArray()) { // O(N)
+            if (c == '(' || c == '{' || c == '[') { // O(1)
+                stack.push(c); // O(1)
             } else {
-                if (stack.isEmpty()) return false;
+                if (stack.isEmpty()) return false; // O(1)
                 
-                char top = stack.pop();
+                char top = stack.pop(); // O(1)
                 
                 if ((c == ')' && top != '(') || 
                     (c == '}' && top != '{') || 
-                    (c == ']' && top != '[')) {
-                    return false;
+                    (c == ']' && top != '[')) { // O(1)
+                    return false; // O(1)
                 }
             }
         }
         
-        return stack.isEmpty();
+        return stack.isEmpty(); // O(1)
     }
 }
