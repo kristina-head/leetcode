1d0e4fe4b4e8aaad4dbdceda8b139d52e0387876
Kristina Head <1kristinahead@gmail.com>
2023-05-08 13:48:26 +0200

    Solution

---
+++ valid-parentheses/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false;
+            
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                
+                char top = stack.pop();
+                
+                if ((c == ')' && top != '(') || 
+                    (c == '}' && top != '{') || 
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
