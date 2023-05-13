86a36eb3b71d393539d60631cf8dcaeaa0b28307
Kristina Head <1kristinahead@gmail.com>
2023-05-13 14:34:14 +0200

    Time complexity analysis

--- evaluate-reverse-polish-notation/Solution.java
+++ evaluate-reverse-polish-notation/Solution.java
@@ -1,28 +1,28 @@
 class Solution {
-    public int evalRPN(String[] tokens) {
-        Deque<Integer> stack = new ArrayDeque<>();
-        int a, b;
+    public int evalRPN(String[] tokens) { // O(N) where N is the number of Strings in `tokens`
+        Deque<Integer> stack = new ArrayDeque<>(); // O(1)
+        int a, b; // O(1)
         
-        for (String token : tokens) {
-            if (token.equals("+")) {
-                stack.push(stack.pop() + stack.pop());
-            } else if (token.equals("-")) {
-                b = stack.pop();
-                a = stack.pop();
+        for (String token : tokens) { // O(N)
+            if (token.equals("+")) { // O(1)
+                stack.push(stack.pop() + stack.pop()); // O(1)
+            } else if (token.equals("-")) { // O(1)
+                b = stack.pop(); // O(1)
+                a = stack.pop(); // O(1)
                 
-                stack.push(a - b);
-            } else if (token.equals("*")) {
-                stack.push(stack.pop() * stack.pop());
-            } else if (token.equals("/")) {
-                b = stack.pop();
-                a = stack.pop();
+                stack.push(a - b); // O(1)
+            } else if (token.equals("*")) { // O(1)
+                stack.push(stack.pop() * stack.pop()); // O(1)
+            } else if (token.equals("/")) { // O(1)
+                b = stack.pop(); // O(1)
+                a = stack.pop(); // O(1)
                 
-                stack.push(a / b);
+                stack.push(a / b); // O(1)
             } else {
-                stack.push(Integer.valueOf(token));
+                stack.push(Integer.valueOf(token)); // O(1)
             }
         }
         
-        return stack.pop();
+        return stack.pop(); // O(1)
     }
 }
