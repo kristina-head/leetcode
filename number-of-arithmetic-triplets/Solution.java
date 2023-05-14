710ae1d007bd5d7afb008d915e807df02d99c3ae
Kristina Head <1kristinahead@gmail.com>
2023-05-14 15:27:25 +0200

    Time complexity

--- number-of-arithmetic-triplets/Solution.java
+++ number-of-arithmetic-triplets/Solution.java
@@ -1,14 +1,14 @@
 class Solution {
-    public int arithmeticTriplets(int[] nums, int diff) {
-        Set<Integer> set = new HashSet<>(nums.length);
-        int count = 0;
+    public int arithmeticTriplets(int[] nums, int diff) { // O(N)
+        Set<Integer> set = new HashSet<>(nums.length); // O(1)
+        int count = 0; // O(1)
 
-        for (int num : nums) {
-            if (set.contains(num - diff) && set.contains(num - diff * 2)) count++;
+        for (int num : nums) { // O(N)
+            if (set.contains(num - diff) && set.contains(num - diff * 2)) count++; // O(1)
 
-            set.add(num);
+            set.add(num); // O(1)
         }
 
-        return count;
+        return count; // O(1)
     }
 }
