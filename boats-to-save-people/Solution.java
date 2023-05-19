11c60efd9049b4b9bd3c9a4104b181252c5612a4
Kristina Head <1kristinahead@gmail.com>
2023-05-19 10:25:31 +0200

    Time complexity analysis

--- boats-to-save-people/Solution.java
+++ boats-to-save-people/Solution.java
@@ -1,16 +1,16 @@
 class Solution {
-    public int numRescueBoats(int[] people, int limit) {
-        Arrays.sort(people);
-        int i = 0;
-        int j = people.length - 1;
-        int boats = 0;
+    public int numRescueBoats(int[] people, int limit) { // O(N log N)
+        Arrays.sort(people); // O(N log N)
+        int i = 0; // O(1)
+        int j = people.length - 1; // O(1)
+        int boats = 0; // O(1)
 
-        while (i <= j) {
-            boats++;
-            if (people[i] + people[j] <= limit) i++;
-            j--;
+        while (i <= j) { // O(N)
+            boats++; // O(1)
+            if (people[i] + people[j] <= limit) i++; // O(1)
+            j--; // O(1)
         }
 
-        return boats;
+        return boats; // O(1)
     }
 }
