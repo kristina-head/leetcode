5847c54ca67e0c21c35bfc7d488de0ec2f686951
Kristina Head <1kristinahead@gmail.com>
2023-05-20 11:35:59 +0200

    Add time complexity for methods

--- design-hashset/MyHashSet.java
+++ design-hashset/MyHashSet.java
@@ -2,7 +2,7 @@ class MyHashSet {
     private Bucket[] buckets;
     private int size;
 
-    public MyHashSet() {
+    public MyHashSet() { // O(769) -> O(1)
         this.size = 769; // Arbitrary prime number, can be adjusted
         this.buckets = new Bucket[this.size];
         for (int i = 0; i < this.size; i++) {
@@ -10,41 +10,41 @@ class MyHashSet {
         }
     }
     
-    public void add(int key) {
+    public void add(int key) { // O(1)
         int index = hash(key);
         buckets[index].insert(key);
     }
     
-    public void remove(int key) {
+    public void remove(int key) { // O(1)
         int index = hash(key);
         buckets[index].delete(key);
     }
     
-    public boolean contains(int key) {
+    public boolean contains(int key) { // O(N)
         int index = hash(key);
         return buckets[index].exists(key);
     }
 
-    private int hash(int key) {
+    private int hash(int key) { // O(1)
         return key % this.size;
     }
 
     static class Bucket {
         private LinkedList<Integer> bucket;
 
-        public Bucket() {
+        public Bucket() { // O(1)
             this.bucket = new LinkedList<Integer>();
         }
 
-        public void insert(Integer key) {
+        public void insert(Integer key) { // O(N)
             if (!exists(key)) this.bucket.addFirst(key);
         }
 
-        public void delete(Integer key) {
+        public void delete(Integer key) { // O(1) because we add to index 0 when inserting
             this.bucket.remove(key);
         }
 
-        public boolean exists(Integer key) {
+        public boolean exists(Integer key) { // O(N)
             int index = this.bucket.indexOf(key);
             return index != -1;
         }
