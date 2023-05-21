a813586869f321846c0ecce1c562f29edf5b7266
Kristina Head <1kristinahead@gmail.com>
2023-05-21 18:08:55 +0200

    Add time complexity for methods

--- design-hashmap/MyHashMap.java
+++ design-hashmap/MyHashMap.java
@@ -2,7 +2,7 @@ class MyHashMap {
     private Bucket[] buckets;
     private int size;
 
-    public MyHashMap() {
+    public MyHashMap() { // O(2069) -> O(1)
         this.size = 2069; // Arbitrary prime number
         this.buckets = new Bucket[this.size];
         for (int i = 0; i < this.size; i++) {
@@ -10,40 +10,40 @@ class MyHashMap {
         }
     }
     
-    public int get(int key) {
+    public int get(int key) { // Amortized O(1)
         int index = hash(key);
         return this.buckets[index].select(key);
     }
 
-    public void put(int key, int value) {
+    public void put(int key, int value) { // Amortized O(1)
         int index = hash(key);
         this.buckets[index].update(key, value);
     }
     
-    public void remove(int key) {
+    public void remove(int key) {  // Amortized O(1)
         int index = hash(key);
         this.buckets[index].delete(key);
     }
 
-    private int hash(int key) {
+    private int hash(int key) { // O(1)
         return key % this.size;
     }
 
     static class Bucket {
         private LinkedList<Pair<Integer, Integer>> bucket;
 
-        public Bucket() {
+        public Bucket() { // O(1)
             this.bucket = new LinkedList<Pair<Integer, Integer>>();
         }
 
-        public Integer select(Integer key) {
+        public Integer select(Integer key) { // Amortized O(1)
             for (Pair<Integer, Integer> pair : this.bucket) {
                 if (pair.key.equals(key)) return pair.value;
             }
             return -1;
         }
 
-        public void update(Integer key, Integer value) {
+        public void update(Integer key, Integer value) { // Amortized O(1)
             boolean found = false;
             for (Pair<Integer, Integer> pair : this.bucket) {
                 if (pair.key.equals(key)) {
@@ -54,7 +54,7 @@ class MyHashMap {
             if (!found) this.bucket.add(new Pair(key, value));
         }
 
-        public void delete(Integer key) {
+        public void delete(Integer key) { // Amortized O(1)
             for (Pair<Integer, Integer> pair : this.bucket) {
                 if (pair.key.equals(key)) {
                     this.bucket.remove(pair);
@@ -68,7 +68,7 @@ class MyHashMap {
         public K key;
         public V value;
 
-        public Pair(K key, V value) {
+        public Pair(K key, V value) { O(1)
             this.key = key;
             this.value = value;
         }
