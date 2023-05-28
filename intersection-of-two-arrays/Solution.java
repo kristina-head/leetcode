ef4b2f5aeb99167c0616e3226766d4b2414c40da
Kristina Head <1kristinahead@gmail.com>
2023-05-28 17:39:36 +0200

    Add time complexity analysis

--- intersection-of-two-arrays/Solution.java
+++ intersection-of-two-arrays/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
+    public int[] intersection(int[] nums1, int[] nums2) { // O(N + M)
+        Set<Integer> set1 = new HashSet<>(); // O(1)
+        Set<Integer> set2 = new HashSet<>(); // O(1)
         
-        for (int num : nums1) set1.add(num);
-        for (int num : nums2) set2.add(num);
+        for (int num : nums1) set1.add(num); // O(N)
+        for (int num : nums2) set2.add(num); // O(M)
         
-        set1.retainAll(set2);
+        set1.retainAll(set2); // O(min(N, M))
         
-        int[] intersections = new int[set1.size()];
-        int i = 0;
-        for (int num : set1) intersections[i++] = num;
+        int[] intersections = new int[set1.size()]; // O(1)
+        int i = 0; // O(1)
+        for (int num : set1) intersections[i++] = num; // O(N) worst case
         
-        return intersections;
+        return intersections; // O(1)
     }
 }
