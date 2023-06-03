502cfc8f0a5204d8a5062557db8f12d6f31d355c
Kristina Head <1kristinahead@gmail.com>
2023-06-03 17:47:57 +0200

    Add time complexity

--- single-row-keyboard/Solution.java
+++ single-row-keyboard/Solution.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int calculateTime(String keyboard, String word) {
-        int[] map = new int[keyboard.length()];
-        for (int i = 0; i < keyboard.length(); i++) {
-            map[keyboard.charAt(i) - 'a'] = i;
+    public int calculateTime(String keyboard, String word) { // O(N) where N is the number of chars in word
+        int[] map = new int[keyboard.length()]; // O(1)
+        for (int i = 0; i < keyboard.length(); i++) { // O(26) -> O(1)
+            map[keyboard.charAt(i) - 'a'] = i; // O(1)
         }
 
-        int prev = 0;
-        int total = 0;
+        int prev = 0; // O(1)
+        int total = 0; // O(1)
 
-        for (char c : word.toCharArray()) {
-            total += Math.abs(prev - map[c - 'a']);
-            prev = map[c - 'a'];
+        for (char c : word.toCharArray()) { // O(N)
+            total += Math.abs(prev - map[c - 'a']); // O(1)
+            prev = map[c - 'a']; // O(1)
         }
 
-        return total;
+        return total; // O(1)
     }
 }
