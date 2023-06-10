e535ca36cd67c953de6a26d9155370c782103765
Kristina Head <1kristinahead@gmail.com>
2023-06-10 19:22:19 +0200

    Add time complexity

--- removing-stars-from-a-string/Solution.java
+++ removing-stars-from-a-string/Solution.java
@@ -1,15 +1,15 @@
 class Solution {
-    public String removeStars(String s) {
-        StringBuilder sb = new StringBuilder();
+    public String removeStars(String s) { // O(N)
+        StringBuilder sb = new StringBuilder(); // O(1)
 
-        for (char c : s.toCharArray()) {
-            if (c == '*') {
-                sb.setLength(sb.length() - 1);
+        for (char c : s.toCharArray()) { // O(N)
+            if (c == '*') { // O(1)
+                sb.setLength(sb.length() - 1); // O(1)
             } else {
-                sb.append(c);
+                sb.append(c); // O(1)
             }
         }
 
-        return sb.toString();
+        return sb.toString(); // O(N)
     }
 }
