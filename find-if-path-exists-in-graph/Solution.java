7c032cb935e855871b4669952ca454cd9aed05f8
Kristina Head <1kristinahead@gmail.com>
2023-06-17 15:07:16 +0200

    Time complexity

--- find-if-path-exists-in-graph/Solution.java
+++ find-if-path-exists-in-graph/Solution.java
@@ -1,30 +1,30 @@
 class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        Map<Integer, ArrayList<Integer>> graph = new HashMap<>();
-        Deque<Integer> queue = new ArrayDeque<>();
-        boolean[] seen = new boolean[n];
+    public boolean validPath(int n, int[][] edges, int source, int destination) { // O(V + E) where V is the number of vertices and E is the number of edges in the graph
+        Map<Integer, ArrayList<Integer>> graph = new HashMap<>(); // O(1)
+        Deque<Integer> queue = new ArrayDeque<>(); // O(1)
+        boolean[] seen = new boolean[n]; // O(1)
         
-        for (int[] edge : edges) {
-            graph.computeIfAbsent(edge[0], it -> new ArrayList<>()).add(edge[1]);
-            graph.computeIfAbsent(edge[1], it -> new ArrayList<>()).add(edge[0]);
+        for (int[] edge : edges) { // O(E)
+            graph.computeIfAbsent(edge[0], it -> new ArrayList<>()).add(edge[1]); // O(1)
+            graph.computeIfAbsent(edge[1], it -> new ArrayList<>()).add(edge[0]); // O(1)
         }
         
-        queue.addLast(source);
-        seen[source] = true;
+        queue.addLast(source); // O(1)
+        seen[source] = true; // O(1)
         
-        while (!queue.isEmpty()) {
-            int current = queue.removeFirst();
-            if (current == destination) return true;
+        while (!queue.isEmpty()) { // O(V + E)
+            int current = queue.removeFirst(); // O(1)
+            if (current == destination) return true; // O(1)
             
-            ArrayList<Integer> vertices = graph.get(current);
-            for (int vertex : vertices) {
-                if (!seen[vertex]) {
-                    queue.addLast(vertex);
-                    seen[vertex] = true;
+            ArrayList<Integer> vertices = graph.get(current); // O(1)
+            for (int vertex : vertices) { // O(E)
+                if (!seen[vertex]) { // O(1)
+                    queue.addLast(vertex); // O(1)
+                    seen[vertex] = true; // O(1)
                 }
             }
         }
         
-        return false;
+        return false; // O(1)
     }
 }
