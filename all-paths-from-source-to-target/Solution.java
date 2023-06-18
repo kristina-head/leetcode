46348db60fcb3927630dc1700aa9c664d3d93b5f
Kristina Head <1kristinahead@gmail.com>
2023-06-18 17:08:35 +0200

    Add time complexity analysis

--- all-paths-from-source-to-target/Solution.java
+++ all-paths-from-source-to-target/Solution.java
@@ -1,25 +1,25 @@
 class Solution {
-    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<List<Integer>> paths = new ArrayList<>();
-        List<Integer> path = new ArrayList<>();
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) { // O(2^N * N) where N is the number of vertices in the graph
+        List<List<Integer>> paths = new ArrayList<>(); // O(1)
+        List<Integer> path = new ArrayList<>(); // O(1)
         
-        dfs(0, graph, path, paths);
+        dfs(0, graph, path, paths); // O(2^N)
         
-        return paths;
+        return paths; // O(1)
     }
     
-    private void dfs(int current, int[][] graph, List<Integer> path, List<List<Integer>> paths) {
-        path.add(current);
+    private void dfs(int current, int[][] graph, List<Integer> path, List<List<Integer>> paths) { // O(2^N)
+        path.add(current); // O(1)
         
-        if (current == graph.length - 1) {
-            paths.add(new ArrayList<>(path));
-            return;
+        if (current == graph.length - 1) { // O(1)
+            paths.add(new ArrayList<>(path)); // O(N)
+            return; // O(1)
         }
         
-        int[] vertices = graph[current];
-        for (int vertex : vertices) {
-            dfs(vertex, graph, path, paths);
-            path.remove(path.size() - 1);
+        int[] vertices = graph[current]; // O(1)
+        for (int vertex : vertices) { // O(N)
+            dfs(vertex, graph, path, paths); // O(2^N)
+            path.remove(path.size() - 1); // O(1)
         }
     }
 }
