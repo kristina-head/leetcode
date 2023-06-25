5005dfb4116202cf1fa5bdf7a396e557139089e5
Kristina Head <1kristinahead@gmail.com>
2023-06-25 09:51:48 +0200

    Add time complexity analysis

--- max-area-of-island/Solution.java
+++ max-area-of-island/Solution.java
@@ -1,34 +1,34 @@
 class Solution {
-    public int maxAreaOfIsland(int[][] grid) {
-        int maxArea = 0;
+    public int maxAreaOfIsland(int[][] grid) { // O(M * N) where M is the number of rows and N is the number of columns in the grid
+        int maxArea = 0; // O(1)
 
-        for (int r = 0; r < grid.length; r++) {
-            for (int c = 0; c < grid[0].length; c++) {
-                if (grid[r][c] == 1) {
-                    int area = dfs(r, c, grid);
-                    maxArea = Math.max(area, maxArea);
+        for (int r = 0; r < grid.length; r++) { // O(M)
+            for (int c = 0; c < grid[0].length; c++) {  // O(N)
+                if (grid[r][c] == 1) { // O(1)
+                    int area = dfs(r, c, grid); // O(1)
+                    maxArea = Math.max(area, maxArea); // O(1)
                 }
             }
         }
 
-        return maxArea;
+        return maxArea; // O(1)
     }
 
-    private int dfs(int r, int c, int[][] grid) {
-        boolean rInBounds = r >= 0 && r < grid.length;
-        boolean cInBounds = c >= 0 && c < grid[0].length;
-        if (!rInBounds || !cInBounds) return 0;
+    private int dfs(int r, int c, int[][] grid) { // O(1)
+        boolean rInBounds = r >= 0 && r < grid.length; // O(1)
+        boolean cInBounds = c >= 0 && c < grid[0].length; // O(1)
+        if (!rInBounds || !cInBounds) return 0; // O(1)
 
-        if (grid[r][c] == 0) return 0;
-        grid[r][c] = 0;
+        if (grid[r][c] == 0) return 0; // O(1)
+        grid[r][c] = 0; // O(1)
 
-        int size = 1;
+        int size = 1; // O(1)
 
-        size += dfs(r + 1, c, grid);
-        size += dfs(r - 1, c, grid);
-        size += dfs(r, c + 1, grid);
-        size += dfs(r, c - 1, grid);
+        size += dfs(r + 1, c, grid); // O(1)
+        size += dfs(r - 1, c, grid); // O(1)
+        size += dfs(r, c + 1, grid); // O(1)
+        size += dfs(r, c - 1, grid); // O(1)
 
-        return size;
+        return size; // O(1)
     }
 }
