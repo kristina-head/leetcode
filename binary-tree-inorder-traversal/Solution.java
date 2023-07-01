a532dbe6839225b2431cdc8c63bf773aa9584927
Kristina Head <1kristinahead@gmail.com>
2023-07-01 11:32:46 +0200

    Time complexity analysis

--- binary-tree-inorder-traversal/Solution.java
+++ binary-tree-inorder-traversal/Solution.java
@@ -14,18 +14,18 @@
  * }
  */
 class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        dfs(root, result);
+    public List<Integer> inorderTraversal(TreeNode root) { // O(N)
+        List<Integer> result = new ArrayList<>(); // O(1)
+        dfs(root, result); // O(N)
         
-        return result;
+        return result; // O(1)
     }
     
-    private void dfs(TreeNode node, List<Integer> result) {
-        if (node == null) return;
+    private void dfs(TreeNode node, List<Integer> result) { // O(N)
+        if (node == null) return; // O(1)
         
-        dfs(node.left, result);
-        result.add(node.val);
-        dfs(node.right, result);
+        dfs(node.left, result); // O(N / 2)
+        result.add(node.val); // O(1)
+        dfs(node.right, result); // O(N / 2)
     }
 }
