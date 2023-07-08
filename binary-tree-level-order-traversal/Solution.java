6bb6dc90c7e7a662e8a5939b78174681a1e483ec
Kristina Head <1kristinahead@gmail.com>
2023-07-08 19:36:46 +0100

    Add definition for TreeNode

--- binary-tree-level-order-traversal/Solution.java
+++ binary-tree-level-order-traversal/Solution.java
@@ -1,3 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) { // O(N)
         List<List<Integer>> levels = new ArrayList<>(); // O(1)
