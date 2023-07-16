80dd8305287eb3576f97e40c7c29c90a257e0bf8
Kristina Head <1kristinahead@gmail.com>
2023-07-16 19:07:55 +0100

    Time complexity analysis

--- isomorphic-strings/Solution.java
+++ isomorphic-strings/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
-    public boolean isIsomorphic(String s, String t) {
-        Map<Character, Character> map = new HashMap<>();
+    public boolean isIsomorphic(String s, String t) { // O(N) where N is the length of s and t
+        Map<Character, Character> map = new HashMap<>(); // O(1)
         
-        for (int i = 0; i < s.length(); i++) {
-            if (map.containsKey(s.charAt(i))) {
-                if (map.get(s.charAt(i)) != t.charAt(i)) return false;
+        for (int i = 0; i < s.length(); i++) { // O(N)
+            if (map.containsKey(s.charAt(i))) { // O(1)
+                if (map.get(s.charAt(i)) != t.charAt(i)) return false; // O(1)
             } else {
-                if (map.containsValue(t.charAt(i))) return false;
+                if (map.containsValue(t.charAt(i))) return false; // O(1)
                 
-                map.put(s.charAt(i), t.charAt(i));
+                map.put(s.charAt(i), t.charAt(i)); // O(1)
             }
         }
         
-        return true;
+        return true; // O(1)
     }
 }
