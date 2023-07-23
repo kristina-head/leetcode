d49d9c6b421b55b9633fd5a42e0ee858591f6e37
Kristina Head <1kristinahead@gmail.com>
2023-07-23 10:10:27 +0100

    Time complexity analysis

--- first-unique-character-in-a-string/Solution.java
+++ first-unique-character-in-a-string/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int firstUniqChar(String s) {
-        int[] charCount = new int[26];
+    public int firstUniqChar(String s) { // O(N) where N is the length of String s
+        int[] charCount = new int[26]; // O(1)
         
-        for (char c : s.toCharArray()) {
-            int index = c - 'a';
-            charCount[index]++;
+        for (char c : s.toCharArray()) { // O(N)
+            int index = c - 'a'; // O(1)
+            charCount[index]++; // O(1)
         }
         
-        for (int i = 0; i < s.length(); i++) {
-            int index = s.charAt(i) - 'a';
-            if (charCount[index] == 1) return i;
+        for (int i = 0; i < s.length(); i++) { // O(N)
+            int index = s.charAt(i) - 'a'; // O(1)
+            if (charCount[index] == 1) return i; // O(1)
         }
         
-        return -1;
+        return -1; // O(1)
     }
 }
