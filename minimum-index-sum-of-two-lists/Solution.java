f0e0a5aeeccafedbdaeb033b3a14826a3394e48f
Kristina Head <1kristinahead@gmail.com>
2023-07-24 10:25:02 +0100

    Time complexity

--- minimum-index-sum-of-two-lists/Solution.java
+++ minimum-index-sum-of-two-lists/Solution.java
@@ -1,27 +1,27 @@
 class Solution {
-    public String[] findRestaurant(String[] list1, String[] list2) {
-        Map<String, Integer> map = new HashMap<>();
-        List<String> result = new ArrayList<>();
-        int smallestIndex = Integer.MAX_VALUE;
+    public String[] findRestaurant(String[] list1, String[] list2) { // O(N + M) where N is list1.length and M is list2.length
+        Map<String, Integer> map = new HashMap<>(); // O(1)
+        List<String> result = new ArrayList<>(); // O(1)
+        int smallestIndex = Integer.MAX_VALUE; // O(1)
         
-        for (int i = 0; i < list1.length; i++) {
-            map.put(list1[i], i);
+        for (int i = 0; i < list1.length; i++) { // O(N)
+            map.put(list1[i], i); // O(1)
         }
         
-        for (int j = 0; j < list2.length; j++) {
-            if (map.containsKey(list2[j])) {
-                int index = j + map.get(list2[j]);
+        for (int j = 0; j < list2.length; j++) { // O(M)
+            if (map.containsKey(list2[j])) { // O(1)
+                int index = j + map.get(list2[j]); // O(1)
                 
-                if (index < smallestIndex) {
-                    smallestIndex = index;
-                    result.clear();
-                    result.add(list2[j]);
-                } else if (index == smallestIndex) {
-                    result.add(list2[j]);
+                if (index < smallestIndex) { // O(1)
+                    smallestIndex = index; // O(1)
+                    result.clear(); // O(N) worst case result contains all Strings from list1
+                    result.add(list2[j]); // O(1)
+                } else if (index == smallestIndex) { // O(1)
+                    result.add(list2[j]); // O(1)
                 }
             }
         }
         
-        return result.toArray(String[]::new);
+        return result.toArray(String[]::new); // O(N) worst case result contains all Strings from list1
     }
 }
