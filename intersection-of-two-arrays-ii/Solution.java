6eb7733db126a51ed343ac64458d14c085fe9940
Kristina Head <1kristinahead@gmail.com>
2023-07-30 16:40:12 +0100

    Add time complexity analysis

--- intersection-of-two-arrays-ii/Solution.java
+++ intersection-of-two-arrays-ii/Solution.java
@@ -1,21 +1,21 @@
 class Solution {
-    public int[] intersect(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> result = new ArrayList<>();
+    public int[] intersect(int[] nums1, int[] nums2) { // O(N + M)
+        Map<Integer, Integer> map = new HashMap<>(); // O(1)
+        List<Integer> result = new ArrayList<>(); // O(1)
         
-        for (int num : nums1) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        for (int num : nums1) { // O(N)
+            map.put(num, map.getOrDefault(num, 0) + 1); // O(1)
         }
         
-        for (int num : nums2) {
-            int val = map.getOrDefault(num, 0);
+        for (int num : nums2) { // O(M)
+            int val = map.getOrDefault(num, 0); // O(1)
             
-            if (val > 0) {
-                result.add(num);
-                map.replace(num, val - 1);
+            if (val > 0) { // O(1)
+                result.add(num); // O(1)
+                map.replace(num, val - 1); // O(1)
             }
         }
         
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        return result.stream().mapToInt(Integer::intValue).toArray(); // O(N) worst case all elements in nums1 appear in nums2
     }
 }
