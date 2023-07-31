8d50177ba97f93e6dcceb36a73aa9a4bb8f0dfa5
Kristina Head <1kristinahead@gmail.com>
2023-07-31 15:12:21 +0100

    Time complexity

--- contains-duplicate-ii/Solution.java
+++ contains-duplicate-ii/Solution.java
@@ -1,25 +1,25 @@
 class Solution {
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer, List<Integer>> indicesByNum = new HashMap<>();
+    public boolean containsNearbyDuplicate(int[] nums, int k) { // O(N^2) worst case and O(N) average case
+        Map<Integer, List<Integer>> indicesByNum = new HashMap<>(); // O(1)
         
-        for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
+        for (int i = 0; i < nums.length; i++) { // O(N)
+            int num = nums[i]; // O(1)
             
-            if (indicesByNum.containsKey(num)) {
-                List<Integer> indices = indicesByNum.get(num);
+            if (indicesByNum.containsKey(num)) { // O(1)
+                List<Integer> indices = indicesByNum.get(num); // O(1)
                 
-                for (int j : indices) {
-                    if (Math.abs(i - j) <= k) return true;
+                for (int j : indices) { // O(N) worst case all elements in nums are the same
+                    if (Math.abs(i - j) <= k) return true; // O(1)
                 }
                 
-                indices.add(i);
+                indices.add(i); // O(1)
             } else {
-                List<Integer> indices = new ArrayList<>();
-                indices.add(i);
-                indicesByNum.put(num, indices);
+                List<Integer> indices = new ArrayList<>(); // O(1)
+                indices.add(i); // O(1)
+                indicesByNum.put(num, indices); // O(1)
             }
         }
         
-        return false;
+        return false; // O(1)
     }
 }
