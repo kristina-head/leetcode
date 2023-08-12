239257c44c72104d4b5bda3726c70bd4cd119e8f
Kristina Head <1kristinahead@gmail.com>
2023-08-12 12:36:23 +0200

    Time complexity analysis

--- average-salary-excluding-the-minimum-and-maximum-salary/Solution.java
+++ average-salary-excluding-the-minimum-and-maximum-salary/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
-    public double average(int[] salary) {
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        double total = 0;
+    public double average(int[] salary) { // O(N)
+        int min = Integer.MAX_VALUE; // O(1)
+        int max = Integer.MIN_VALUE; // O(1)
+        double total = 0; // O(1)
         
-        for (int amount : salary) {
-            min = Math.min(min, amount);
-            max = Math.max(max, amount);
-            total += amount;
+        for (int amount : salary) { // O(N)
+            min = Math.min(min, amount); // O(1)
+            max = Math.max(max, amount); // O(1)
+            total += amount; // O(1)
         }
 
-        total -= min + max;
+        total -= min + max; // O(1)
         
-        return total / (salary.length - 2);
+        return total / (salary.length - 2); // O(1)
     }
 }
