58a4e3fc22147781337eb015f266c5372f841b32
Kristina Head <1kristinahead@gmail.com>
2023-08-13 17:26:18 +0200

    Time complexity analysis

--- group-anagrams/Solution.java
+++ group-anagrams/Solution.java
@@ -1,13 +1,13 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> groupedAnagrams = new HashMap<>();
+    public List<List<String>> groupAnagrams(String[] strs) { // O(N * M log M) where N is the number of Strings in strs and M is the length of the String
+        Map<String, List<String>> groupedAnagrams = new HashMap<>(); // O(1)
         
-        for (String s : strs) {
-            char[] chars = s.toCharArray();
-            Arrays.sort(chars);
-            groupedAnagrams.computeIfAbsent(new String(chars), it -> new ArrayList<>()).add(s);
+        for (String s : strs) { // O(N)
+            char[] chars = s.toCharArray(); // O(M)
+            Arrays.sort(chars); // O(M log M)
+            groupedAnagrams.computeIfAbsent(new String(chars), it -> new ArrayList<>()).add(s); // O(M)
         }
         
-        return new ArrayList<>(groupedAnagrams.values());
+        return new ArrayList<>(groupedAnagrams.values()); // O(N)
     }
 }
