d24274d47ec355c9489d911b9875fc169d9781fc
Kristina Head <1kristinahead@gmail.com>
2023-08-19 16:23:06 +0200

    Time complexity analysis

--- group-shifted-strings/Solution.java
+++ group-shifted-strings/Solution.java
@@ -1,23 +1,23 @@
 class Solution {
-    public List<List<String>> groupStrings(String[] strings) {
-        Map<String, List<String>> sequences = new HashMap<>();
+    public List<List<String>> groupStrings(String[] strings) { // O(N * M) where N is the number of Strings in strings and M is the length of each String
+        Map<String, List<String>> sequences = new HashMap<>(); // O(1)
         
-        for (String s : strings) {
-            String key = getHash(s);
-            sequences.computeIfAbsent(key, it -> new ArrayList<>()).add(s);
+        for (String s : strings) { // O(N)
+            String key = getHash(s); // O(M)
+            sequences.computeIfAbsent(key, it -> new ArrayList<>()).add(s); // O(1)
         }
         
-        return new ArrayList<>(sequences.values());
+        return new ArrayList<>(sequences.values()); // O(N)
     }
 
-    private String getHash(String s) {
-        char[] chars = s.toCharArray();
-        StringBuilder hash = new StringBuilder();
+    private String getHash(String s) { // O(M)
+        char[] chars = s.toCharArray(); // O(M)
+        StringBuilder hash = new StringBuilder(); // O(1)
 
-        for (int i = 1; i < chars.length; i++) {
-            hash.append((chars[i] - chars[i - 1] + 26) % 26 + 'a');
+        for (int i = 1; i < chars.length; i++) { // O(M)
+            hash.append((chars[i] - chars[i - 1] + 26) % 26 + 'a'); // O(1)
         }
 
-        return hash.toString();
+        return hash.toString(); // O(M)
     }
 }
