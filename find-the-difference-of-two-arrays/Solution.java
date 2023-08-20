7149e02308032077f7687857e31c73ae573bb387
Kristina Head <1kristinahead@gmail.com>
2023-08-20 15:19:41 +0200

    Add time complexity analysis

--- find-the-difference-of-two-arrays/Solution.java
+++ find-the-difference-of-two-arrays/Solution.java
@@ -1,24 +1,24 @@
 class Solution {
-    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) { // O(N + M)
+        Set<Integer> set1 = new HashSet<>(); // O(1)
+        Set<Integer> set2 = new HashSet<>(); // O(1)
 
-        for (int num : nums1) {
-            set1.add(num);
+        for (int num : nums1) { // O(N)
+            set1.add(num); // O(1)
         }
 
-        for (int num : nums2) {
-            set2.add(num);
+        for (int num : nums2) { // O(M)
+            set2.add(num); // O(1)
         }
 
-        for (int num : nums1) {
-            set2.remove(num);
+        for (int num : nums1) { // O(N)
+            set2.remove(num); // O(1)
         }
 
-        for (int num : nums2) {
-            set1.remove(num);
+        for (int num : nums2) { // O(M)
+            set1.remove(num); // O(1)
         }
 
-        return List.of(new ArrayList(set1), new ArrayList(set2));
+        return List.of(new ArrayList(set1), new ArrayList(set2)); // O(N + M)
     }
 }
