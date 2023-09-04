0faa34226479a295b4c494de860775e082524d4c
Kristina Head <1kristinahead@gmail.com>
2023-09-04 08:35:25 +0200

    Time complexity

--- valid-anagram/Solution.java
+++ valid-anagram/Solution.java
@@ -1,23 +1,23 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        int[] charFrequency = new int[26];
+    public boolean isAnagram(String s, String t) { // O(N) where N is the length of both input Strings
+        if (s.length() != t.length()) return false; // O(1)
+        int[] charFrequency = new int[26]; // O(1)
 
-        for (char c : s.toCharArray()) {
-            int index = c - 'a';
-            charFrequency[index]++;
+        for (char c : s.toCharArray()) { // O(N)
+            int index = c - 'a'; // O(1)
+            charFrequency[index]++; // O(1)
         }
 
-        for (char c : t.toCharArray()) {
-            int index = c - 'a';
-            if (charFrequency[index] == 0) return false;
-            charFrequency[index]--;
+        for (char c : t.toCharArray()) { // O(N)
+            int index = c - 'a'; // O(1)
+            if (charFrequency[index] == 0) return false; // O(1)
+            charFrequency[index]--; // O(1)
         }
 
-        for (int i : charFrequency) {
-            if (i > 0) return false;
+        for (int i : charFrequency) { // O(26) -> O(1)
+            if (i > 0) return false; // O(1)
         }
 
-        return true;
+        return true; // O(1)
     }
 }
