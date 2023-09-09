2c943c49b6f76e6a55d0c97133d5f7615531b118
Kristina Head <1kristinahead@gmail.com>
2023-09-09 13:53:49 +0200

    Use enhanced for loops

--- jewels-and-stones/Solution.java
+++ jewels-and-stones/Solution.java
@@ -1,16 +1,14 @@
 class Solution {
   public int numJewelsInStones(String J, String S) { // O(M + N)
     Set<Character> jewels = new HashSet<>(); // O(1)
+    int count = 0; // O(1)
     
-    for (int i = 0; i < J.length(); i++) { // O(M)
-      jewels.add(J.charAt(i)); // O(1)
+    for (char jewel : J.toCharArray()) { // O(M)
+      jewels.add(jewel); // O(1)
     }
     
-    int count = 0; // O(1)
-    for (int k = 0; k < S.length(); k++) { // O(N)
-      if (jewels.contains(S.charAt(k))) { // O(1)
-        count++; // O(1)
-      }
+    for (char stone : S.toCharArray()) { // O(N)
+      if (jewels.contains(stone)) count++; // O(1)
     }
     
     return count; // O(1)
