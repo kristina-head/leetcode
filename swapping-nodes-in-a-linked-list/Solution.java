7b810793b17e86af2370e913c56b6d532356e336
Kristina Head <1kristinahead@gmail.com>
2023-09-10 09:49:41 +0200

    Time complexity

--- swapping-nodes-in-a-linked-list/Solution.java
+++ swapping-nodes-in-a-linked-list/Solution.java
@@ -9,25 +9,25 @@
  * }
  */
 class Solution {
-    public ListNode swapNodes(ListNode head, int k) {
-        ListNode kthFromBeginning = head;
-        ListNode kthFromEnd = head;
-        ListNode node = head;
-        int size = 0;
+    public ListNode swapNodes(ListNode head, int k) { // O(N)
+        ListNode kthFromBeginning = head; // O(1)
+        ListNode kthFromEnd = head; // O(1)
+        ListNode node = head; // O(1)
+        int size = 0; // O(1)
 
-        while (node != null) {
-            if (++size == k) kthFromBeginning = node;
-            node = node.next;
+        while (node != null) { // O(N)
+            if (++size == k) kthFromBeginning = node; // O(1)
+            node = node.next; // O(1)
         }
 
-        for (int i = 1; i <= size - k; i++) {
-            kthFromEnd = kthFromEnd.next;
+        for (int i = 1; i <= size - k; i++) { // O(N)
+            kthFromEnd = kthFromEnd.next; // O(1)
         }
 
-        int temp = kthFromBeginning.val;
-        kthFromBeginning.val = kthFromEnd.val;
-        kthFromEnd.val = temp;
+        int temp = kthFromBeginning.val; // O(1)
+        kthFromBeginning.val = kthFromEnd.val; // O(1)
+        kthFromEnd.val = temp; // O(1)
 
-        return head;
+        return head; // O(1)
     }
 }
