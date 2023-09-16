370af80adb7ba326300a2692417133d1904e8269
Kristina Head <1kristinahead@gmail.com>
2023-09-16 10:28:50 +0200

    Add time complexity analysis

--- swap-nodes-in-pairs/Solution.java
+++ swap-nodes-in-pairs/Solution.java
@@ -9,28 +9,28 @@
  * }
  */
 class Solution {
-    public ListNode swapPairs(ListNode head) {
-        if (head == null || head.next == null) return head;
+    public ListNode swapPairs(ListNode head) { // O(N) where N is the number of ListNodes in the linked list
+        if (head == null || head.next == null) return head; // O(1)
 
-        ListNode first = head;
-        ListNode second = head.next;
-        ListNode newHead = second;
+        ListNode first = head; // O(1)
+        ListNode second = head.next; // O(1)
+        ListNode newHead = second; // O(1)
 
-        while (second != null) {
-            ListNode temp = second.next;
-            second.next = first;
+        while (second != null) { // O(N)
+            ListNode temp = second.next; // O(1)
+            second.next = first; // O(1)
 
-            if (temp == null || temp.next == null) {
-                first.next = temp;
-                break;
+            if (temp == null || temp.next == null) { // O(1)
+                first.next = temp; // O(1)
+                break; // O(1)
             }
 
-            first.next = temp.next;
+            first.next = temp.next; // O(1)
             
-            first = temp;
-            second = first.next;
+            first = temp; // O(1)
+            second = first.next; // O(1)
         }
 
-        return newHead;
+        return newHead; // O(1)
     }
 }
