e632c71ce699bd85ec61f2f46a0d00f5c3470f30
Kristina Head <1kristinahead@gmail.com>
2023-09-23 11:30:49 +0200

    Time complexity analysis

--- minimum-number-of-vertices-to-reach-all-nodes/Solution.java
+++ minimum-number-of-vertices-to-reach-all-nodes/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
-    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
-        boolean[] visited = new boolean[n];
-        List<Integer> unvisited = new ArrayList<>();
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) { // O(E + V) where E is the number of edges and V is the number of vertices in the graph
+        boolean[] visited = new boolean[n]; // O(1)
+        List<Integer> unvisited = new ArrayList<>(); // O(1)
 
-        for (int i = 0; i < edges.size(); i++) {
-            int verticeWithIncomingEdge = edges.get(i).get(1);
-            visited[verticeWithIncomingEdge] = true;
+        for (int i = 0; i < edges.size(); i++) { // O(E)
+            int verticeWithIncomingEdge = edges.get(i).get(1); // O(1)
+            visited[verticeWithIncomingEdge] = true; // O(1)
         }
 
-        for (int i = 0; i < visited.length; i++) {
-            if (!visited[i]) unvisited.add(i);
+        for (int i = 0; i < visited.length; i++) { // O(V)
+            if (!visited[i]) unvisited.add(i); // O(1)
         }
 
-        return unvisited;
+        return unvisited; // O(1)
     }
 }
