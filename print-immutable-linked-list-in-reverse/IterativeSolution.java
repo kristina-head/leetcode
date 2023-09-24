97ea3b811c05d533c944133744cbcbf47b7cac6d
Kristina Head <1kristinahead@gmail.com>
2023-09-24 09:38:21 +0200

    Time complexity analysis

--- print-immutable-linked-list-in-reverse/IterativeSolution.java
+++ print-immutable-linked-list-in-reverse/IterativeSolution.java
@@ -1,14 +1,14 @@
 class IterativeSolution {
-    public void printLinkedListInReverse(ImmutableListNode head) {
-        Deque<ImmutableListNode> stack = new ArrayDeque<>();
+    public void printLinkedListInReverse(ImmutableListNode head) { // O(N)
+        Deque<ImmutableListNode> stack = new ArrayDeque<>(); // O(1)
 
-        while (head != null) {
-            stack.push(head);
-            head = head.getNext();
+        while (head != null) { // O(N)
+            stack.push(head); // O(1)
+            head = head.getNext(); // O(1)
         }
 
-        while (!stack.isEmpty()) {
-            stack.pop().printValue();
+        while (!stack.isEmpty()) { // O(N)
+            stack.pop().printValue(); // O(1)
         }
     }
 }
