d24b422c3ef9c73a40fa1af7f481a9cd059d9fb9
Kristina Head <1kristinahead@gmail.com>
2023-09-25 12:24:37 +0200

    Implement SparseVector class

---
+++ dot-product-of-two-sparse-vectors/SparseVector.java
@@ -0,0 +1,34 @@
+/**
+ * A sparse vector is a vector that has mostly zero values.
+ */
+class SparseVector {
+    private Map<Integer, Integer> compressedVector;
+    
+    /**
+     * Because the vector is sparse, initializes a Map to only store the indices and values of non-zero numbers.
+     */
+    SparseVector(int[] nums) {
+        this.compressedVector = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                this.compressedVector.put(i, nums[i]);
+            }
+        }
+    }
+    
+    /**
+     * Returns the dotProduct of two sparse vectors.
+     */
+    public int dotProduct(SparseVector vec) {
+        int product = 0;
+
+        for (Integer i : this.compressedVector.keySet()) {
+            if (vec.compressedVector.containsKey(i)) {
+                product += this.compressedVector.get(i) * vec.compressedVector.get(i);
+            }
+        }
+
+        return product;
+    }
+}
