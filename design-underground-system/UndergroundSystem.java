686851a7977472383d0b362dbc278eeb75dacfc7
Kristina Head <1kristinahead@gmail.com>
2023-09-30 09:31:43 +0200

    Add time complexity for each method

--- design-underground-system/UndergroundSystem.java
+++ design-underground-system/UndergroundSystem.java
@@ -2,16 +2,16 @@ class UndergroundSystem {
     private Map<Integer, CheckInData> checkInData;
     private Map<String, JourneyData> journeyData;
 
-    public UndergroundSystem() {
+    public UndergroundSystem() { // O(1)
         this.checkInData = new HashMap<>();
         this.journeyData = new HashMap<>();
     }
     
-    public void checkIn(int id, String stationName, int t) {
+    public void checkIn(int id, String stationName, int t) { // O(1)
         checkInData.put(id, new CheckInData(stationName, t));
     }
     
-    public void checkOut(int id, String stationName, int t) {
+    public void checkOut(int id, String stationName, int t) { // O(N)
         CheckInData checkInDataForId = checkInData.get(id);
         String route = getRoute(checkInDataForId.stationName, stationName);
         JourneyData journeyDataForRoute = journeyData.getOrDefault(route, new JourneyData());
@@ -23,14 +23,14 @@ class UndergroundSystem {
         checkInData.remove(id);
     }
     
-    public double getAverageTime(String startStation, String endStation) {
+    public double getAverageTime(String startStation, String endStation) { // O(N)
         String route = getRoute(startStation, endStation);
         JourneyData journeyDataForRoute = journeyData.get(route);
 
         return journeyDataForRoute.totalTripTime / journeyDataForRoute.totalTrips;
     }
 
-    private String getRoute(String startStation, String endStation) {
+    private String getRoute(String startStation, String endStation) { // O(N) where N is the combined station lengths
         StringBuilder route = new StringBuilder();
 
         route.append(startStation);
@@ -44,7 +44,7 @@ class UndergroundSystem {
         private String stationName;
         private int time;
 
-        public CheckInData(String stationName, int time) {
+        public CheckInData(String stationName, int time) { // O(1)
             this.stationName = stationName;
             this.time = time;
         }
@@ -54,7 +54,7 @@ class UndergroundSystem {
         private double totalTripTime;
         private double totalTrips;
 
-        public JourneyData() {
+        public JourneyData() { // O(1)
             this.totalTripTime = 0.0;
             this.totalTrips = 0.0;
         }
