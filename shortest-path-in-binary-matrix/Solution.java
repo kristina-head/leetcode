651ad88c51180bf4b86fa746f72b54ae44a45da1
Kristina Head <1kristinahead@gmail.com>
2023-10-01 09:34:24 +0200

    Fix formatting and add time complexity

--- shortest-path-in-binary-matrix/Solution.java
+++ shortest-path-in-binary-matrix/Solution.java
@@ -1,55 +1,55 @@
 class Solution {
-    private static final int[][] directions = new int[][]{
+    private static final int[][] directions = new int[][] {
         {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}
-    };
+    }; // O(1)
 
-    public int shortestPathBinaryMatrix(int[][] grid) {
-        int n = grid.length;
+    public int shortestPathBinaryMatrix(int[][] grid) { // O(N^2)
+        int n = grid.length; // O(1)
 
-        if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0) {
-            return -1;
+        if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0) { // O(1)
+            return -1; // O(1)
         }
 
-        Deque<int[]> queue = new ArrayDeque<>();
-        grid[0][0] = 1;
-        queue.addLast(new int[] {0, 0});
+        Deque<int[]> queue = new ArrayDeque<>(); // O(1)
+        grid[0][0] = 1; // O(1)
+        queue.addLast(new int[] {0, 0}); // O(1)
 
-        while (!queue.isEmpty()) {
-            int[] cell = queue.removeFirst();
-            int row = cell[0];
-            int col = cell[1];
-            int distance = grid[row][col];
+        while (!queue.isEmpty()) { // O(N^2)
+            int[] cell = queue.removeFirst(); // O(1)
+            int row = cell[0]; // O(1)
+            int col = cell[1]; // O(1)
+            int distance = grid[row][col]; // O(1)
 
-            if (row == n - 1 && col == n - 1) {
-                    return distance;
-                }
+            if (row == n - 1 && col == n - 1) { // O(1)
+                return distance; // O(1)
+            }
 
-            for (int[] neighbor : getNeighbors(grid, row, col)) {
-                int neighborRow = neighbor[0];
-                int neighborCol = neighbor[1];
+            for (int[] neighbor : getNeighbors(grid, row, col)) { // O(8) -> O(1)
+                int neighborRow = neighbor[0]; // O(1)
+                int neighborCol = neighbor[1]; // O(1)
                 
-                queue.addLast(new int[] {neighborRow, neighborCol});
-                grid[neighborRow][neighborCol] = distance + 1;
+                queue.addLast(new int[] {neighborRow, neighborCol}); // O(1)
+                grid[neighborRow][neighborCol] = distance + 1; // O(1)
             }
         }
 
-        return -1;
+        return -1; // O(1)
     }
 
-    private List<int[]> getNeighbors(int[][] grid, int row, int col) {
-        List<int[]> neighbors = new ArrayList<>();
+    private List<int[]> getNeighbors(int[][] grid, int row, int col) { // O(1)
+        List<int[]> neighbors = new ArrayList<>(); // O(1)
 
-        for (int[] direction : directions) {
-            int neighborRow = row + direction[0];
-            int neighborCol = col + direction[1];
-            boolean neighborRowInBounds = neighborRow >= 0 && neighborRow < grid.length;
-            boolean neighborColInBounds = neighborCol >= 0 && neighborCol < grid.length;
+        for (int[] direction : directions) { // O(8) -> O(1)
+            int neighborRow = row + direction[0]; // O(1)
+            int neighborCol = col + direction[1]; // O(1)
+            boolean neighborRowInBounds = neighborRow >= 0 && neighborRow < grid.length; // O(1)
+            boolean neighborColInBounds = neighborCol >= 0 && neighborCol < grid.length; // O(1)
 
-            if (neighborRowInBounds && neighborColInBounds && grid[neighborRow][neighborCol] == 0) {
-                neighbors.add(new int[] {neighborRow, neighborCol});
+            if (neighborRowInBounds && neighborColInBounds && grid[neighborRow][neighborCol] == 0) { // O(1)
+                neighbors.add(new int[] {neighborRow, neighborCol}); // O(1)
             }
         }
 
-        return neighbors;
+        return neighbors; // O(1)
     }
 }
