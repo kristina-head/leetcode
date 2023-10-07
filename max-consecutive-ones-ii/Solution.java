d83e10c43bf7492e5c315cc8d263175ae819169b
Kristina Head <1kristinahead@gmail.com>
2023-10-07 17:08:26 +0200

    Time complexity

--- max-consecutive-ones-ii/Solution.java
+++ max-consecutive-ones-ii/Solution.java
@@ -1,19 +1,19 @@
 class Solution {
-    public int findMaxConsecutiveOnes(int[] nums) {
-        int maxConsecutiveOnes = 0;
-        int prev = 0, current = 0;
+    public int findMaxConsecutiveOnes(int[] nums) { // O(N)
+        int maxConsecutiveOnes = 0; // O(1)
+        int prev = 0, current = 0; // O(1)
         
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                prev = current + 1;
-                current = 0;
+        for (int i = 0; i < nums.length; i++) { // O(N)
+            if (nums[i] == 0) { // O(1)
+                prev = current + 1; // O(1)
+                current = 0; // O(1)
             } else {
-                current++;
+                current++; // O(1)
             }
             
-            maxConsecutiveOnes = Math.max(maxConsecutiveOnes, prev + current);
+            maxConsecutiveOnes = Math.max(maxConsecutiveOnes, prev + current); // O(1)
         }
         
-        return maxConsecutiveOnes;
+        return maxConsecutiveOnes; // O(1)
     }
 }
