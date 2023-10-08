efb392552aa91d54620abbc4daea5c7df4736d3c
Kristina Head <1kristinahead@gmail.com>
2023-10-08 09:44:12 +0200

    Time complexity analysis

--- pascals-triangle/Solution.java
+++ pascals-triangle/Solution.java
@@ -1,23 +1,23 @@
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> pascalsTriangle = new ArrayList<>();
+    public List<List<Integer>> generate(int numRows) { // O(N^2) 
+        List<List<Integer>> pascalsTriangle = new ArrayList<>(); // O(1)
         
-        for (int i = 1; i <= numRows; i++) {
-            List<Integer> row = new ArrayList<>();
+        for (int i = 1; i <= numRows; i++) { // O(N)
+            List<Integer> row = new ArrayList<>(); // O(1)
             
-            for (int j = 0; j < i; j++) {
-                if (j == 0 || j == i - 1) {
-                    row.add(1);
+            for (int j = 0; j < i; j++) { // O(i) -> O(N)
+                if (j == 0 || j == i - 1) { // O(1)
+                    row.add(1); // O(1)
                 } else {
-                    List<Integer> prevRow = pascalsTriangle.get(i - 2);
-                    int sum = prevRow.get(j - 1) + prevRow.get(j);
-                    row.add(sum);
+                    List<Integer> prevRow = pascalsTriangle.get(i - 2); // O(1)
+                    int sum = prevRow.get(j - 1) + prevRow.get(j); // O(1)
+                    row.add(sum); // O(1)
                 }
             }
             
-            pascalsTriangle.add(row);
+            pascalsTriangle.add(row); // O(1)
         }
         
-        return pascalsTriangle;
+        return pascalsTriangle; // O(1)
     }
 }
