f56112992b85f5ac629dd5f0ef5a531d812a3b96
Kristina Head <1kristinahead@gmail.com>
2023-10-09 08:37:21 +0200

    Time complexity

--- add-binary/Solution.java
+++ add-binary/Solution.java
@@ -1,23 +1,23 @@
 class Solution {
-    public String addBinary(String a, String b) {
-        int n = a.length() - 1, m = b.length() - 1;
-        if (n < m) return addBinary(b, a);
+    public String addBinary(String a, String b) { // O(N) where N is the longer input String
+        int n = a.length() - 1, m = b.length() - 1; // O(1)
+        if (n < m) return addBinary(b, a); // O(N)
         
-        StringBuilder sum = new StringBuilder();
-        int carry = 0;
+        StringBuilder sum = new StringBuilder(); // O(1)
+        int carry = 0; // O(1)
 
-        while (n >= 0) {
-            if (a.charAt(n--) == '1') carry++;
-            if (m >= 0 && b.charAt(m--) == '1') carry++;
+        while (n >= 0) { // O(N)
+            if (a.charAt(n--) == '1') carry++; // O(1)
+            if (m >= 0 && b.charAt(m--) == '1') carry++; // O(1)
 
-            char bit = (carry % 2 == 1) ? '1' : '0';
-            sum.insert(0, bit);
+            char bit = (carry % 2 == 1) ? '1' : '0'; // O(1)
+            sum.insert(0, bit); // O(1)
 
-            carry /= 2;
+            carry /= 2; // O(1)
         }
 
-        if (carry == 1) sum.insert(0, '1');
+        if (carry == 1) sum.insert(0, '1'); // O(1)
         
-        return sum.toString();
+        return sum.toString(); // O(N)
     }
 }
