aa014e241a5c1c8c9cd249a9f3e025d04efb4783
Kristina Head <1kristinahead@gmail.com>
2023-10-16 10:31:03 +0200

    Time complexity analysis

--- find-smallest-letter-greater-than-target/BinarySearchSolution.java
+++ find-smallest-letter-greater-than-target/BinarySearchSolution.java
@@ -1,17 +1,17 @@
 class BinarySearchSolution {
-    public char nextGreatestLetter(char[] letters, char target) {
-        int start = 0, end = letters.length - 1;
+    public char nextGreatestLetter(char[] letters, char target) { // O(log N)
+        int start = 0, end = letters.length - 1; // O(1)
 
-        while (start <= end) {
-            int mid = (start + end) / 2;
+        while (start <= end) { // O(log N)
+            int mid = (start + end) / 2; // O(1)
 
-            if (letters[mid] <= target) {
-                start = mid + 1;
+            if (letters[mid] <= target) { // O(1)
+                start = mid + 1; // O(1)
             } else {
-                end = mid - 1;
+                end = mid - 1; // O(1)
             }
         }
 
-        return start == letters.length ? letters[0] : letters[start];
+        return start == letters.length ? letters[0] : letters[start]; // O(1)
     }
 }
