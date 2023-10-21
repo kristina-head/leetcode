b3305be195484a822780ccc0fac5cb8fb30dcc8a
Kristina Head <1kristinahead@gmail.com>
2023-10-21 09:21:02 +0200

    Add time complexity analysis

--- array-partition/Solution.java
+++ array-partition/Solution.java
@@ -1,12 +1,12 @@
 class Solution {
-    public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
-        int pairSum = 0;
+    public int arrayPairSum(int[] nums) { // O(N log N)
+        Arrays.sort(nums); // O(N log N)
+        int pairSum = 0; // O(1)
         
-        for (int i = 0; i < nums.length; i += 2) {
-            pairSum += nums[i];
+        for (int i = 0; i < nums.length; i += 2) { // O(N / 2) -> O(N)
+            pairSum += nums[i]; // O(1)
         }
         
-        return pairSum;
+        return pairSum; // O(1)
     }
 }
