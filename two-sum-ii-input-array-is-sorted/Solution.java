4b50b5e131c72e4a1bc6292e150fb2084ec001b0
Kristina Head <1kristinahead@gmail.com>
2023-10-22 10:41:04 +0200

    Time complexity analysis

--- two-sum-ii-input-array-is-sorted/Solution.java
+++ two-sum-ii-input-array-is-sorted/Solution.java
@@ -1,20 +1,20 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        int i = 0;
-        int j = numbers.length - 1;
+    public int[] twoSum(int[] numbers, int target) { // O(N)
+        int i = 0; // O(1)
+        int j = numbers.length - 1; // O(1)
         
-        while (i < j) {
-            int sum = numbers[i] + numbers[j];
+        while (i < j) { // O(N)
+            int sum = numbers[i] + numbers[j]; // O(1)
 
-            if (sum == target) {
-                return new int[] { i + 1, j + 1 };
-            } else if (sum < target) {
-                i++;
+            if (sum == target) { // O(1)
+                return new int[] { i + 1, j + 1 }; // O(1)
+            } else if (sum < target) { // O(1)
+                i++; // O(1)
             } else {
-                j--;
+                j--; // O(1)
             }
         }
         
-        return new int[] { -1, -1 };
+        return new int[] { -1, -1 }; // O(1)
     }
 }
