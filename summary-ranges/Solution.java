c3864f142ed5e53ca17ca53983c2eaa2718ddf56
Kristina Head <1kristinahead@gmail.com>
2023-10-29 12:13:22 +0100

    Add time complexity

--- summary-ranges/Solution.java
+++ summary-ranges/Solution.java
@@ -1,19 +1,19 @@
 class Solution {
-    public List<String> summaryRanges(int[] nums) {
-        List<String> ranges = new ArrayList<>();
+    public List<String> summaryRanges(int[] nums) { // O(N)
+        List<String> ranges = new ArrayList<>(); // O(1)
 
-        for (int i = 0; i < nums.length; i++) {
-            int start = nums[i];
+        for (int i = 0; i < nums.length; i++) { // O(N)
+            int start = nums[i]; // O(1)
 
-            while (i < nums.length - 1 && nums[i] + 1 == nums[i + 1]) i++;
+            while (i < nums.length - 1 && nums[i] + 1 == nums[i + 1]) i++; // O(N)
 
-            if (start != nums[i]) {
-                ranges.add(start + "->" + nums[i]);
+            if (start != nums[i]) { // O(1)
+                ranges.add(start + "->" + nums[i]); // O(1)
             } else {
-                ranges.add(String.valueOf(start));
+                ranges.add(String.valueOf(start)); // O(log N)
             }
         }
 
-        return ranges;
+        return ranges; // O(1)
     }
 }
