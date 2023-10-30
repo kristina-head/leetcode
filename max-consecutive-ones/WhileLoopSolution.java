97b62639e546a54de46ff4a0b39709587497ecb3
Kristina Head <1kristinahead@gmail.com>
2023-10-30 10:46:59 +0100

    Time complexity

--- max-consecutive-ones/WhileLoopSolution.java
+++ max-consecutive-ones/WhileLoopSolution.java
@@ -1,21 +1,21 @@
 class Solution {
-    public int findMaxConsecutiveOnes(int[] nums) {
-        if (nums.length == 1) return nums[0];
+    public int findMaxConsecutiveOnes(int[] nums) { // O(N)
+        if (nums.length == 1) return nums[0]; // O(1)
         
-        int n = nums.length;
-        int maxConsecutiveOnes = 0;
-        int left = 0, right = 0;
+        int n = nums.length; // O(1)
+        int maxConsecutiveOnes = 0; // O(1)
+        int left = 0, right = 0; // O(1)
         
-        while (left < n && right < n) {
-            while (left < n && nums[left] == 0) left++;
-            right = left;
+        while (left < n && right < n) { // O(N)
+            while (left < n && nums[left] == 0) left++; // O(N)
+            right = left; // O(1)
 
-            while (right < n && nums[right] == 1) right++;
+            while (right < n && nums[right] == 1) right++; // O(N)
 
-            maxConsecutiveOnes = Math.max(maxConsecutiveOnes, right = left);
-            left = right;
+            maxConsecutiveOnes = Math.max(maxConsecutiveOnes, right = left); // O(1)
+            left = right; // O(1)
         }
         
-        return maxConsecutiveOnes;
+        return maxConsecutiveOnes; // O(1)
     }
 }
