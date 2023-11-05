4b95384a6211f860506d03e90cab4845d873b3e4
Kristina Head <1kristinahead@gmail.com>
2023-11-05 14:34:36 +0100

    Time complexity analysis

--- minimum-absolute-difference-in-bst/Solution.java
+++ minimum-absolute-difference-in-bst/Solution.java
@@ -14,26 +14,26 @@
  * }
  */
 class Solution {   
-    public int getMinimumDifference(TreeNode root) {
-        List<Integer> values = new ArrayList<>();
-        getValues(root, values);
+    public int getMinimumDifference(TreeNode root) { // O(N)
+        List<Integer> values = new ArrayList<>(); // O(1)
+        getValues(root, values); // O(N)
         
-        int minDiff = Integer.MAX_VALUE;
-        for (int i = 1; i < values.size(); i++) {
-            minDiff = Math.min(minDiff, values.get(i) - values.get(i - 1));
+        int minDiff = Integer.MAX_VALUE; // O(1)
+        for (int i = 1; i < values.size(); i++) { // O(N)
+            minDiff = Math.min(minDiff, values.get(i) - values.get(i - 1)); // O(1)
         }
 
-        return minDiff;
+        return minDiff; // O(1)
     }
 
     /**
      * Performs an in order traversal so values are added in ascending order.
      */
-    private void getValues(TreeNode node, List<Integer> values) {
-        if (node == null) return;
+    private void getValues(TreeNode node, List<Integer> values) { // O(N)
+        if (node == null) return; // O(1)
 
-        getValues(node.left, values);
-        values.add(node.val);
-        getValues(node.right, values);
+        getValues(node.left, values); // O(N / 2)
+        values.add(node.val); // O(1)
+        getValues(node.right, values); // O(N / 2)
     }
 }
