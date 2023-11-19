c2006f2e89554c33129acfdc04417682bb888001
Kristina Head <1kristinahead@gmail.com>
2023-11-19 09:27:25 +0100

    Time complexity analysis

--- search-in-rotated-sorted-array/Solution.java
+++ search-in-rotated-sorted-array/Solution.java
@@ -1,28 +1,28 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int start = 0, end = nums.length - 1;
-        if (target < nums[start] && target > nums[end]) return -1;
+    public int search(int[] nums, int target) { // O(log N)
+        int start = 0, end = nums.length - 1; // O(1)
+        if (target < nums[start] && target > nums[end]) return -1; // O(1)
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+        while (start <= end) { // O(log N)
+            int mid = start + (end - start) / 2; // O(1)
 
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] >= nums[start]) {
-                if (target >= nums[start] && target < nums[mid]) {
-                    end = mid - 1;
+            if (nums[mid] == target) { // O(1)
+                return mid; // O(1)
+            } else if (nums[mid] >= nums[start]) { // O(1)
+                if (target >= nums[start] && target < nums[mid]) { // O(1)
+                    end = mid - 1; // O(1)
                 } else {
-                    start = mid + 1;
+                    start = mid + 1; // O(1)
                 } 
             } else {
-                if (target <= nums[end] && target > nums[mid]) {
-                    start = mid + 1;
+                if (target <= nums[end] && target > nums[mid]) { // O(1)
+                    start = mid + 1; // O(1)
                 } else {
-                    end = mid - 1;
+                    end = mid - 1; // O(1)
                 }
             }
         }
 
-        return -1;
+        return -1; // O(1)
     }
 }
