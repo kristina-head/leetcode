160d8cc2b0d6df22afe1a370f5f9ec81361e99df
Kristina Head <1kristinahead@gmail.com>
2023-11-26 12:21:00 +0100

    Time complexity

--- find-peak-element/Solution.java
+++ find-peak-element/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int findPeakElement(int[] nums) {
-        int left = 0, right = nums.length - 1;
+    public int findPeakElement(int[] nums) { // O(log N)
+        int left = 0, right = nums.length - 1; // O(1)
         
-        while (left < right) {
-            int mid = left + (right - left) / 2;
+        while (left < right) { // O(log N)
+            int mid = left + (right - left) / 2; // O(1)
             
-            if (nums[mid] < nums[mid + 1]) {
-                left = mid + 1;
+            if (nums[mid] < nums[mid + 1]) { // O(1)
+                left = mid + 1; // O(1)
             } else {
-                right = mid;
+                right = mid; // O(1)
             }
         }
         
-        return left;
+        return left; // O(1)
     }
 }
