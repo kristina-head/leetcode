ea95c7b51eb01de780fc0f6a32ed7dd27477df45
Kristina Head <1kristinahead@gmail.com>
2024-02-03 14:22:11 +0100

    Time complexity analysis

--- find-minimum-in-rotated-sorted-array/Solution.java
+++ find-minimum-in-rotated-sorted-array/Solution.java
@@ -1,27 +1,27 @@
 class Solution {
-    public int findMin(int[] nums) {
-        int n = nums.length - 1;
-        int left = 0, right = n;
-        if (nums[left] < nums[right]) return nums[0];
+    public int findMin(int[] nums) { // O(log N)
+        int n = nums.length - 1; // O(1)
+        int left = 0, right = n; // O(1)
+        if (nums[left] < nums[right]) return nums[0]; // O(1)
         
-        while (left < right) {
-            int mid = left + (right - left) / 2;
+        while (left < right) { // O(log N)
+            int mid = left + (right - left) / 2; // O(1)
             
-            if (nums[mid] > nums[mid + 1]) {
-                return nums[mid + 1];
+            if (nums[mid] > nums[mid + 1]) { // O(1)
+                return nums[mid + 1]; // O(1)
             }
             
-            if (nums[mid - 1] > nums[mid]) {
-                return nums[mid];
+            if (nums[mid - 1] > nums[mid]) { // O(1)
+                return nums[mid]; // O(1)
             }
             
-            if (nums[mid] > nums[0]) {
-                left = mid + 1;
+            if (nums[mid] > nums[0]) { // O(1)
+                left = mid + 1; // O(1)
             } else {
-                right = mid - 1;
+                right = mid - 1; // O(1)
             }
         }
         
-        return nums[0];
+        return nums[0]; // O(1)
     }
 }
